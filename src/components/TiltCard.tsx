e71995eef875d173757d0d5783f87539e0527848
nicon44 <nicolas@dulio.com.ar>
2024-05-09 09:48:33 +0200

    change on tilt

--- src/components/TiltCard.tsx
+++ src/components/TiltCard.tsx
@@ -10,10 +10,10 @@ interface ICardProps {
 
 const TILT_OPTIONS = {
   reverse: true, // reverse the tilt direction
-  max: 20, // max tilt rotation (degrees)
+  max: 30, // max tilt rotation (degrees)
   perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
   scale: 1.05, // 2 = 200%, 1.5 = 150%, etc..
-  speed: 1000, // Speed of the enter/exit transition
+  speed: 800, // Speed of the enter/exit transition
   transition: true, // Set a transition on enter/exit.
   axis: null, // What axis should be disabled. Can be X or Y.
   reset: true, // If the tilt effect has to be reset on exit.
