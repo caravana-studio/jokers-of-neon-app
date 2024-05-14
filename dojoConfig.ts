e0ea4e0743d31bd6bd40c0a3547eef91437124b6
nicon44 <nicolas@dulio.com.ar>
2024-05-14 19:55:18 +0200

    env

--- dojoConfig.ts
+++ dojoConfig.ts
@@ -1,6 +1,10 @@
-import manifest from "./src/manifest.json";
 import { createDojoConfig } from "@dojoengine/core";
+import manifest from "./src/manifest.json";
+
+const rpcUrl = import.meta.env.VITE_RPC_URL || "http://localhost:5050"
+const toriiUrl = import.meta.env.VITE_TORII_URL || "http://localhost:8080"
+const relayUrl = import.meta.env.VITE_RELAY_URL || ""
 
 export const dojoConfig = createDojoConfig({
-    manifest,
+    manifest, rpcUrl, toriiUrl, relayUrl
 });
--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -42,7 +42,6 @@ export const ActionButton = ({
           backgroundColor: "black",
         },
         "&:disabled": {
-
           backgroundColor: "black",
         },
         ...extraSx,
