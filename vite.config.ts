71b7189e4ee08febf98ca4375a4e5f0b4864d87e
nicon44 <nicolas@dulio.com.ar>
2024-05-14 22:44:59 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
-import { defineConfig } from "vite";
-import wasm from "vite-plugin-wasm";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import topLevelAwait from "vite-plugin-top-level-await";
+import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), wasm(), topLevelAwait()],
     build: {
-      outDir: 'dist'
+      outDir: 'dist',
+      rollupOptions: {
+        external: ["react", "react-router", "react-router-dom"],
+        output: {
+          globals: {
+            react: "React",
+          },
+        },
+      },
     },
 });
