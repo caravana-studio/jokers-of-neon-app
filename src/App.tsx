1d0e3ef04da79c781b0e9c27a1d3382d7a52b43b
nicon44 <nicolas@dulio.com.ar>
2024-06-06 22:09:03 +0200

    remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,55 @@
-import { Box, ChakraBaseProvider, extendBaseTheme, extendTheme } from "@chakra-ui/react";
+import { Box, ChakraBaseProvider, extendTheme } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
-import { GameContent } from "./pages/Game/GameContent";
+import { GamePage } from "./pages/Game/GamePage";
 import { GameOver } from "./pages/GameOver";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Poc } from "./pages/Poc";
 import { CardAnimationsProvider } from "./providers/CardAnimationsProvider";
-import { StaticCardsProvider } from "./providers/StaticCardsProvider";
 import customTheme from "./theme/theme";
-import { GamePage } from "./pages/Game/GamePage";
 
 const theme = extendTheme(customTheme);
 
 function App() {
   return (
     <ChakraBaseProvider theme={theme}>
-      <StaticCardsProvider>
-        <CardAnimationsProvider>
-          <main className="scanlines">
-            <div className="screen">
-              <canvas id="canvas" className="picture"></canvas>
-              <div className="overlay">
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/gameover" element={<GameOver />} />
-                  <Route
-                    path="/demo"
-                    element={
-                      <Box
-                        sx={{
-                          backgroundImage: "url(arcade-neon.gif)",
-                          boxShadow: "inset 0 0 0 1000px rgba(0,0,0,.3)",
-                          backgroundSize: "cover",
-                          backgroundPosition: "center",
-                          height: "100vh",
-                          width: "100vw",
-                          imageRendering: "pixelated !important",
-                          fontFamily: "Sys",
-                          pointerEvents: "all",
-                          color: "#FFF",
-                        }}
-                      >
-                        <GamePage />
-                      </Box>
-                    }
-                  />
-                  <Route path="/poc" element={<Poc />} />
-                </Routes>
-              </div>
+      <CardAnimationsProvider>
+        <main className="scanlines">
+          <div className="screen">
+            <canvas id="canvas" className="picture"></canvas>
+            <div className="overlay">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/gameover" element={<GameOver />} />
+                <Route
+                  path="/demo"
+                  element={
+                    <Box
+                      sx={{
+                        backgroundImage: "url(arcade-neon.gif)",
+                        boxShadow: "inset 0 0 0 1000px rgba(0,0,0,.3)",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        height: "100vh",
+                        width: "100vw",
+                        imageRendering: "pixelated !important",
+                        fontFamily: "Sys",
+                        pointerEvents: "all",
+                        color: "#FFF",
+                      }}
+                    >
+                      <GamePage />
+                    </Box>
+                  }
+                />
+                <Route path="/poc" element={<Poc />} />
+              </Routes>
             </div>
-          </main>
-        </CardAnimationsProvider>
-      </StaticCardsProvider>
+          </div>
+        </main>
+      </CardAnimationsProvider>
     </ChakraBaseProvider>
   );
 }
