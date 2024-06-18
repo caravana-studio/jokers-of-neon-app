8681748d60ee34ffd719d85df39da78593d699b1
Agustin Castro <agustin.castro2011@gmail.com>
2024-06-17 22:47:50 -0300

    deck-2 - size logic

--- src/components/Deck/DeckModal.tsx
+++ src/components/Deck/DeckModal.tsx
@@ -18,7 +18,7 @@ export const DeckModal = ({isOpen, onClose, deck}: DeckModalProps) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose} size="4xl" scrollBehavior="inside" isCentered>
       <ModalOverlay />
-      <ModalContent width="80%" marginTop="15vh">
+      <ModalContent width="90%" marginTop="3%" maxWidth="90%" maxHeight="90%">
         <ModalBody p={0}>
           <DeckOverview commonCards={deck.commonCards} effectCards={deck.effectCards} />
         </ModalBody>
