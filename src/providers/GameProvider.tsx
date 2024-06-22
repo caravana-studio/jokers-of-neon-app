8fc69701525ca9ade844cb6e06f11b5553b23193
Agustin Castro <agustin.castro2011@gmail.com>
2024-06-22 13:20:18 -0300

    fix the roundRewards state when clicking on new game

--- src/providers/GameProvider.tsx
+++ src/providers/GameProvider.tsx
@@ -224,6 +224,7 @@ export const GameProvider = ({ children }: PropsWithChildren) => {
         console.log(`game ${newGameId} created`);
         setGameLoading(false);
         setPreSelectionLocked(false);
+        setRoundRewards(undefined);
       } else {
         setError(true);
       }
