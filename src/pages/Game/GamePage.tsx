f409a476d2f9d40a55dc90c1db38e280e80729cd
nicon44 <nicolas@dulio.com.ar>
2024-06-29 20:40:53 +0200

    fullscreen attempt

--- src/pages/Game/GamePage.tsx
+++ src/pages/Game/GamePage.tsx
@@ -1,18 +1,18 @@
 import { Button, Flex } from "@chakra-ui/react";
 import { isMobile } from "react-device-detect";
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 import { GameContent } from "./GameContent";
 
 export const GamePage = () => {
-  return isMobile && !document.fullscreenElement ? (
+  const handle = useFullScreenHandle();
+  return isMobile && !handle.active ? (
     <Flex justifyContent="center" alignItems="center" height={"100%"}>
-      <Button
-        onClick={() => {
-          document.documentElement.requestFullscreen();
-        }}
-      >
-        FULLSCREEN
-      </Button>
+      <Button onClick={handle.enter}>FULLSCREEN</Button>
     </Flex>
+  ) : isMobile ? (
+    <FullScreen handle={handle}>
+      <GameContent />
+    </FullScreen>
   ) : (
     <GameContent />
   );
