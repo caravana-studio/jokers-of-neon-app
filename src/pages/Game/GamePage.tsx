e08dbd3a67bf4673425d5bf5aee2d3205d2ffc40
nicon44 <nicolas@dulio.com.ar>
2024-06-29 20:28:06 +0200

    fullscreen on mobile

--- src/pages/Game/GamePage.tsx
+++ src/pages/Game/GamePage.tsx
@@ -1,7 +1,19 @@
+import { Button, Flex } from "@chakra-ui/react";
+import { isMobile } from "react-device-detect";
 import { GameContent } from "./GameContent";
 
 export const GamePage = () => {
-  return (
-      <GameContent />
+  return isMobile && !document.fullscreenElement ? (
+    <Flex justifyContent="center" alignItems="center" height={"100%"}>
+      <Button
+        onClick={() => {
+          document.documentElement.requestFullscreen();
+        }}
+      >
+        FULLSCREEN
+      </Button>
+    </Flex>
+  ) : (
+    <GameContent />
   );
 };
