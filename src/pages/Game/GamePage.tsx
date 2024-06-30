9bc65e0ef8f3694dc55290093e7000072582842f
nicon44 <nicolas@dulio.com.ar>
2024-06-30 19:35:04 +0200

    remove scroll

--- src/pages/Game/GamePage.tsx
+++ src/pages/Game/GamePage.tsx
@@ -1,7 +1,13 @@
+import { RemoveScroll } from "react-remove-scroll";
 import { GameContent } from "./GameContent";
 
 export const GamePage = () => {
   return (
+    <>
       <GameContent />
+      <RemoveScroll>
+        <></>
+      </RemoveScroll>
+    </>
   );
 };
