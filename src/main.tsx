a1e4817b3b81d011bc1811a7202b2c015366a1a5
nicon44 <nicolas@dulio.com.ar>
2024-07-01 23:37:59 +0200

    loading screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,47 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "sonner";
 import { dojoConfig } from "../dojoConfig.ts";
 import App from "./App.tsx";
 import { DojoProvider } from "./dojo/DojoContext.tsx";
 import { setup } from "./dojo/generated/setup.ts";
 import "./index.css";
-import { Toaster } from 'sonner'
+import { LoadingScreen } from "./pages/LoadingScreen.tsx";
 
 async function init() {
   const rootElement = document.getElementById("root");
   if (!rootElement) throw new Error("React root not found");
   const root = ReactDOM.createRoot(rootElement as HTMLElement);
 
-  const setupResult = await setup(dojoConfig);
-  const queryClient = new QueryClient();
-
   root.render(
-    <DojoProvider value={setupResult}>
-      <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <Toaster/>
-          <App />
-        </QueryClientProvider>
-      </BrowserRouter>
-    </DojoProvider>
+    <ChakraProvider>
+      <LoadingScreen />
+    </ChakraProvider>    
   );
+
+  try {
+    const setupResult = await setup(dojoConfig);
+    const queryClient = new QueryClient();
+    root.render(
+      <DojoProvider value={setupResult}>
+        <BrowserRouter>
+          <QueryClientProvider client={queryClient}>
+            <Toaster />
+            <App />
+          </QueryClientProvider>
+        </BrowserRouter>
+      </DojoProvider>
+    );
+  } catch {
+    root.render(
+      <ChakraProvider>
+        <LoadingScreen error />
+      </ChakraProvider>    
+    );
+  }
+
 }
 
 init();
