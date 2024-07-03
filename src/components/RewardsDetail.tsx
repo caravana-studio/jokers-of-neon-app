1d597e19b9f38c81a4c2a18a7463709e419f2431
nicon44 <nicolas@dulio.com.ar>
2024-07-03 10:26:38 +0200

    small fixes

--- src/components/RewardsDetail.tsx
+++ src/components/RewardsDetail.tsx
@@ -1,7 +1,7 @@
 import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import { VIOLET } from "../theme/colors";
 import { RoundRewards } from "../types/RoundRewards.ts";
-import { NEON_PINK, VIOLET } from "../theme/colors";
 
 interface RewardItemProps {
   label: string;
@@ -10,11 +10,7 @@ interface RewardItemProps {
 
 const RewardItem = ({ label, value }: RewardItemProps) => {
   return (
-    <Box
-      color="white"
-      px={[2, 4, 8]}
-      w="100%"
-    >
+    <Box color="white" px={[2, 4, 8]} w="100%">
       <Flex
         mt={2}
         pt={1}
@@ -31,16 +27,13 @@ const RewardItem = ({ label, value }: RewardItemProps) => {
             width: "100%",
             height: "1px",
             background: "white",
-            boxShadow: "0 0 1px 0px rgba(255, 255, 255), 0 0 8px 1px rgba(255, 255, 255)"
-          }
+            boxShadow:
+              "0 0 1px 0px rgba(255, 255, 255), 0 0 8px 1px rgba(255, 255, 255)",
+          },
         }}
       >
-        <Text>
-          {label.toUpperCase()}
-        </Text>
-        <Text>
-          {value}Ȼ
-        </Text>
+        <Text>{label.toUpperCase()}</Text>
+        <Text>{value}Ȼ</Text>
       </Flex>
     </Box>
   );
@@ -74,37 +67,34 @@ export const RewardsDetail = ({ roundRewards }: RewardsDetailProps) => {
   const navigate = useNavigate();
 
   return (
-    <Box display="flex" flexDirection="column" alignItems="center" w="80%" maxW="550px" fontFamily="Orbitron">
-      <Heading size="lg" fontWeight={""} color={VIOLET}>CONGRATS!</Heading>
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      w="80%"
+      maxW="550px"
+      fontFamily="Orbitron"
+    >
+      <Heading size="lg" variant="italic" color={VIOLET}>
+        CONGRATS!
+      </Heading>
       <Box
         w="100%"
         px={[1, 2, 4]}
         py={2}
         mt={8}
         border="2px solid #DAA1E8FF"
-        boxShadow= {`0px 0px 20px 15px ${VIOLET}`}
-        filter= "blur(0.5px)"
-        backgroundColor= "rgba(0, 0, 0, 1)"
-        borderRadius= "10px"
+        boxShadow={`0px 0px 20px 15px ${VIOLET}`}
+        filter="blur(0.5px)"
+        backgroundColor="rgba(0, 0, 0, 1)"
+        borderRadius="10px"
         display="grid"
         justifyItems="center"
       >
-        <RewardItem
-          label={labels[0]}
-          value={round_defeat}
-        />
-        <RewardItem
-          label={labels[1]}
-          value={level_bonus}
-        />
-        <RewardItem
-          label={labels[2]}
-          value={hands_left_cash}
-        />
-        <RewardItem
-          label={labels[3]}
-          value={discard_left_cash}
-        />
+        <RewardItem label={labels[0]} value={round_defeat} />
+        <RewardItem label={labels[1]} value={level_bonus} />
+        <RewardItem label={labels[2]} value={hands_left_cash} />
+        <RewardItem label={labels[3]} value={discard_left_cash} />
 
         <Flex
           color={VIOLET}
@@ -115,22 +105,19 @@ export const RewardsDetail = ({ roundRewards }: RewardsDetailProps) => {
           fontWeight="bold"
           fontSize={["lg", "xl", "2xl"]}
         >
-          <Text>
-            Total:
-          </Text>
-          <Text>
-            {total}ȼ
-          </Text>
+          <Text>Total:</Text>
+          <Text>{total}ȼ</Text>
         </Flex>
       </Box>
 
       <Button
-        mt={8}
+        mt={10}
         w="100%"
         size="md"
+        variant="secondarySolid"
         onClick={() => navigate("/redirect/store")}
       >
-        Continue
+        CONTINUE
       </Button>
     </Box>
   );
--- src/pages/RewardsPage.tsx
+++ src/pages/RewardsPage.tsx
@@ -1,31 +1,20 @@
 import { Box, Image } from "@chakra-ui/react";
-import { useEffect } from "react";
+import { isMobile } from "react-device-detect";
+import { useNavigate } from "react-router-dom";
 import { Background } from "../components/Background";
-import { preloadImages } from "../utils/preloadImages";
 import { RewardsDetail } from "../components/RewardsDetail";
 import { useGameContext } from "../providers/GameProvider";
-import { useNavigate } from "react-router-dom";
 
 export const RewardsPage = () => {
   const { roundRewards } = useGameContext();
   const navigate = useNavigate();
 
   if (!roundRewards) {
-    navigate("/")
+    navigate("/store");
   }
 
-  useEffect(() => {
-    preloadImages()
-      .then(() => {
-        console.log("All images preloaded");
-      })
-      .catch((err) => {
-        console.error("Error preloading images:", err);
-      });
-  }, []);
-
   return (
-    <Background type="home">
+    <Background type="game">
       <Box sx={{ width: "100%", height: "100%" }}>
         <Box
           height="15%"
@@ -38,19 +27,21 @@ export const RewardsPage = () => {
           <Image
             alignSelf="center"
             justifySelf="end"
-            src="/logos/joker-logo.png"
-            alt="/logos/joker-logo.png"
-            width={"25%"}
-            maxW={"150px"}
-          />
-          <Image
-            alignSelf="center"
-            justifySelf="end"
             src="/logos/logo-variant.png"
             alt="/logos/logo-variant.png"
             width={"65%"}
             maxW={"400px"}
           />
+          {!isMobile && (
+            <Image
+              alignSelf="center"
+              justifySelf="end"
+              src="/logos/joker-logo.png"
+              alt="/logos/joker-logo.png"
+              width={"25%"}
+              maxW={"150px"}
+            />
+          )}
         </Box>
         <Box
           sx={{
@@ -71,12 +62,14 @@ export const RewardsPage = () => {
             justifyContent: "center",
           }}
         >
-          <Image
-            src="/logos/jn-logo.png"
-            alt="/logos/jn-logo.png"
-            width={"50%"}
-            maxW={"250px"}
-          />
+          {!isMobile && (
+            <Image
+              src="/logos/jn-logo.png"
+              alt="/logos/jn-logo.png"
+              width={"50%"}
+              maxW={"250px"}
+            />
+          )}
         </Box>
       </Box>
     </Background>
