e2046c291b850550afa65ce0737580dec03ef198
nicon44 <nicolas@dulio.com.ar>
2024-07-04 12:44:24 +0200

    game over page

--- src/pages/GameOver.tsx
+++ src/pages/GameOver.tsx
@@ -1,8 +1,10 @@
-import { Box, Button, Flex } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Image } from "@chakra-ui/react";
 import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
+import { isMobile } from "react-device-detect";
 import { useNavigate } from "react-router-dom";
+import { Background } from "../components/Background";
 import { Leaderboard } from "../components/Leaderboard";
 import { GAME_ID, LAST_GAME_ID } from "../constants/localStorage";
 import { getLSGameId } from "../dojo/utils/getLSGameId";
@@ -28,64 +30,77 @@ export const GameOver = () => {
   }, []);
 
   return (
-    <Box
-      sx={{
-        height: "100%",
-        width: "100%",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        animation: "5s ease 2000ms normal none infinite running glitch",
-      }}
-    >
+    <Background type="home">
       <Box
-        sx={{
-          fontSize: "120px",
-          whiteSpace: "nowrap",
-          width: "720px",
-        }}
-        className="text"
+      position='fixed'
+        height="15%"
+        width="100%"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        padding="0 50px"
       >
-        <span>GAME OVER</span>
-        <span>GAME OVER</span>
-        <span>GAME OVER</span>
-        <span>GAME OVER</span>
-        <span>GAME OVER</span>
+        <Image
+          alignSelf="center"
+          justifySelf="end"
+          src="/logos/logo-variant.png"
+          alt="/logos/logo-variant.png"
+          width={"65%"}
+          maxW={"350px"}
+        />
+        {!isMobile && (
+          <Image
+            alignSelf="center"
+            justifySelf="end"
+            src="/logos/joker-logo.png"
+            alt="/logos/joker-logo.png"
+            width={"25%"}
+            maxW={"150px"}
+          />
+        )}
       </Box>
-      <Box sx={{ mt: "180px" }}>
-        <Leaderboard gameId={lastGameId} lines={4} />
-      </Box>
-      <Button
-        sx={{ mt: 4 }}
-        onClick={() => {
-          window.open(
-            `https://twitter.com/intent/tweet?text=%20%F0%9F%94%A5%20Just%20scored%20${score}%20points%20in%20Jokers%20of%20Neon!%20%F0%9F%94%A5%20Think%20you%20can%20beat%20me?%20Give%20it%20a%20shot%20at%20${GAME_URL}%20%F0%9F%83%8F%E2%9C%A8`,
-            "_blank"
-          );
-        }}
-        data-size="large"
+      <Flex
+        height="100%"
+        justifyContent="center"
+        flexDirection="column"
+        alignItems="center"
+        gap={4}
       >
-        Share on
-        <Flex sx={{ ml: 2.5 }}>
-          <FontAwesomeIcon fontSize={22} icon={faXTwitter} />
-        </Flex>
-      </Button>
-      <Button
-        size="l"
-        variant="outline"
-        sx={{
-          mt: 10,
-          backgroundColor: "transparent",
-        }}
-        onClick={() => {
-          localStorage.removeItem(GAME_ID);
-          restartGame();
-          navigate("/demo");
-        }}
-      >
-        START NEW GAME
-      </Button>
-    </Box>
+        <Box>
+          <Heading size="l" variant="italic" textAlign={"center"} mb={12}>
+            GAME OVER
+          </Heading>
+          <Leaderboard gameId={lastGameId} lines={4} />
+        </Box>
+        <Button
+          sx={{ mt: 4 }}
+          onClick={() => {
+            window.open(
+              `https://twitter.com/intent/tweet?text=%20%F0%9F%94%A5%20Just%20scored%20${score}%20points%20in%20Jokers%20of%20Neon!%20%F0%9F%94%A5%20Think%20you%20can%20beat%20me?%20Give%20it%20a%20shot%20at%20${GAME_URL}%20%F0%9F%83%8F%E2%9C%A8`,
+              "_blank"
+            );
+          }}
+          data-size="large"
+        >
+          SHARE YOUR SCORE ON
+          <Flex sx={{ ml: 2.5 }}>
+            <FontAwesomeIcon fontSize={22} icon={faXTwitter} />
+          </Flex>
+        </Button>
+        <Button
+          variant="secondarySolid"
+          sx={{
+            mt: 10,
+          }}
+          onClick={() => {
+            localStorage.removeItem(GAME_ID);
+            restartGame();
+            navigate("/demo");
+          }}
+        >
+          START NEW GAME
+        </Button>
+      </Flex>
+    </Background>
   );
 };
--- src/theme/text.ts
+++ src/theme/text.ts
@@ -21,7 +21,7 @@ export const textTheme = {
     xl: { fontSize: { base: 14, sm: 20, md: 24, lg: 30 }, fontWeight: 600 },
     l: { fontSize: { base: 10, sm: 16, md: 19, lg: 24 } },
     m: { fontSize: { base: 10, sm: 12, md: 14, lg: 18 } },
-    s: { fontSize: { base: 8, sm: 11, md: 15, lg: 17 }},
+    s: { fontSize: { base: 8, sm: 11, md: 18, lg: 20 }},
   },
   defaultProps: {
     size: "m",
