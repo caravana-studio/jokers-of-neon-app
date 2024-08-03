15a83bef8bfc38ade7a3280873f82889b7f43bac
brendamareco <brendaamareco@gmail.com>
2024-08-03 19:25:41 -0300

    Added borders and captions

--- src/pages/RewardsPage.tsx
+++ src/pages/RewardsPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Image } from "@chakra-ui/react";
+import { Box, Image, Text } from "@chakra-ui/react";
 import { isMobile } from "react-device-detect";
 import { useNavigate } from "react-router-dom";
 import { Background } from "../components/Background";
@@ -16,13 +16,23 @@ export const RewardsPage = () => {
   return (
     <Background type="game" dark>
       <Box sx={{ width: "100%", height: "100%" }}>
+        {!isMobile && (
+          <Image 
+            src='/borders/top.png' 
+            height="8%" 
+            width="100%" 
+            maxHeight="70px" 
+            object-fit 
+            position='fixed' 
+            top={0} />
+        )}
         <Box
           height="15%"
           width="100%"
           display="flex"
           justifyContent={isMobile ? "center" : "space-between"}
           alignItems="center"
-          padding="0 50px"
+          padding={isMobile ? "0 50px" : "25px 50px 0px 50px"}
         >
           <Image
             alignSelf="center"
@@ -54,6 +64,36 @@ export const RewardsPage = () => {
         >
           <RewardsDetail roundRewards={roundRewards} />
         </Box>
+        {!isMobile && (
+          <>
+            <Image
+              src='/borders/bottom.png'
+              height="8%"
+              width="100%"
+              maxHeight="70px"
+              object-fit
+              position='fixed'
+              bottom={0} />
+              <Box
+                sx={{
+                  position: "fixed",
+                  bottom: 16,
+                  left: 12,
+                }}
+              >
+                <Text size="m">BUIDL YOUR DECK</Text>
+              </Box>
+              <Box
+                sx={{
+                  position: "fixed",
+                  bottom: 16,
+                  right: 12,
+                }}
+              >
+                <Text size="m">RULE THE GAME</Text>
+              </Box>
+          </>
+        )}
       </Box>
     </Background>
   );
