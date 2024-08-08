0dbe99dc0eef7a5de592d11cda72c2e6fff2bc6c
Cristian Fleita <cristianagustinfleita@gmail.com>
2024-08-08 14:48:26 -0300

    Updated install prompt style

--- src/utils/InstallPromptModal.tsx
+++ src/utils/InstallPromptModal.tsx
@@ -21,26 +21,28 @@ interface InstallPromptModalProps {
 
 const InstallPromptModal: React.FC<InstallPromptModalProps> = ({ isOpen, onClose, onInstall, onPlayInBrowser }) => {
   return (
-    <Modal size="xl" isOpen={isOpen} onClose={onClose}>
+    <Modal size="xxl" isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
-      <ModalContent>
+      <ModalContent padding={{ base:4, sm:12 }}>
         <ModalHeader>
-          <Heading size="lg" variant="italic">
-            Jokers of Neon
+          <Heading size={{ base: "m", sm: "lg" }}variant="italic">
+          App install
           </Heading>
         </ModalHeader>
         <ModalCloseButton />
         <ModalBody>
           <Flex flexDirection="column" alignItems="center" gap={4}>
-            <Text fontSize="lg" textAlign="center">
-              Best way to play Jokers of Neon is by installing our App. Do you want to install the app now?
+            <Text fontSize={{ base: "s", sm: "lg" }} textAlign="center">
+            Don't miss out on the ultimate Jokers of Neon experience. Install our app now!
             </Text>
-            <Flex gap={4}>
+            <Flex gap={{ base: 4, sm: 6 }}
+              flexWrap={{ base: "wrap", sm: "nowrap" }}
+              justifyContent="center" paddingTop={4}>
               <Button colorScheme="blue" onClick={onInstall}>
                 Install app
               </Button>
               <Button variant="secondarySolid" onClick={onPlayInBrowser}>
-                Play on the browser
+                Keep Playing on browser
               </Button>
             </Flex>
           </Flex>
