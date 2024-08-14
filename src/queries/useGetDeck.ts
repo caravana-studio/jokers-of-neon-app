4cef4967c0330e7ff0c39cd7586fcf45dc106756
nicon44 <nicolas@dulio.com.ar>
2024-08-14 16:59:43 -0400

    remove unused code

--- src/queries/useGetDeck.ts
+++ src/queries/useGetDeck.ts
@@ -1,13 +1,31 @@
+import { gql } from "graphql-tag";
 import { useQuery } from "react-query";
 import graphQLClient from "../graphQLClient";
-import { GET_DECK_QUERY } from "./gqlQueries"
-import { Deck } from '../types/Deck.ts'
+import { Deck } from "../types/Deck.ts";
 
 export const DECK_QUERY_KEY = "deck";
 
+const GET_DECK_QUERY = gql`
+  query GetDeck($gameId: ID!) {
+    jokersOfNeonRoundModels(where: { game_idEQ: $gameId }) {
+      edges {
+        node {
+          current_len_deck
+        }
+      }
+    }
+    jokersOfNeonPlayerCommonCardsModels(where: { game_idEQ: $gameId }) {
+      totalCount
+    }
+    jokersOfNeonPlayerEffectCardsModels(where: { game_idEQ: $gameId }) {
+      totalCount
+    }
+  }
+`;
+
 interface RoundEdge {
   node: {
-    current_len_deck: number
+    current_len_deck: number;
   };
 }
 
@@ -23,12 +41,9 @@ interface DeckQueryResponse {
   };
 }
 
-const fetchGraphQLData = async (
-  gameId: number
-): Promise<DeckQueryResponse> => {
+const fetchGraphQLData = async (gameId: number): Promise<DeckQueryResponse> => {
   return await graphQLClient.request(GET_DECK_QUERY, { gameId });
 };
-
 
 export const useGetDeck = (gameId: number) => {
   const queryResponse = useQuery<DeckQueryResponse>(
@@ -38,15 +53,17 @@ export const useGetDeck = (gameId: number) => {
   );
   const { data } = queryResponse;
 
-  const deckLength = data?.jokersOfNeonRoundModels?.edges[0]?.node?.current_len_deck ?? 0;
+  const deckLength =
+    data?.jokersOfNeonRoundModels?.edges[0]?.node?.current_len_deck ?? 0;
 
-  const deckSize = (data?.jokersOfNeonPlayerCommonCardsModels?.totalCount ?? 0)
-                            + (data?.jokersOfNeonPlayerEffectCardsModels?.totalCount ?? 0);
+  const deckSize =
+    (data?.jokersOfNeonPlayerCommonCardsModels?.totalCount ?? 0) +
+    (data?.jokersOfNeonPlayerEffectCardsModels?.totalCount ?? 0);
 
   const deck: Deck = {
     currentLength: deckLength,
-    size: deckSize
-  }
+    size: deckSize,
+  };
   return {
     ...queryResponse,
     data: deck,
