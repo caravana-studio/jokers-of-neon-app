352bad776046670ad6af9f0c3806c0f222d1dd9d
nicon44 <nicolas@dulio.com.ar>
2024-08-20 17:59:06 -0400

    improve purchased items state mgmt

--- src/providers/StoreProvider.tsx
+++ src/providers/StoreProvider.tsx
@@ -1,35 +1,69 @@
 import { PropsWithChildren, createContext, useContext, useState } from "react";
 import { useDojo } from "../dojo/useDojo";
+import { Card } from "../types/Card";
+import { PokerHandItem } from "../types/PokerHandItem";
+import { getCardType } from "../utils/getCardType";
+import { getCardUniqueId } from "../utils/getCardUniqueId";
 import { useGameContext } from "./GameProvider";
 
 interface IStoreContext {
-  buyCard: (
-    card_idx: number,
-    card_type: number,
-    price: number
-  ) => Promise<boolean>;
-  levelUpPlay: (item_id: number, price: number) => Promise<boolean>;
+  buyCard: (card: Card) => Promise<boolean>;
+  levelUpPlay: (item: PokerHandItem) => Promise<boolean>;
   reroll: () => Promise<boolean>;
   locked: boolean;
+  isPurchased: (card: Card | PokerHandItem) => boolean;
 }
 
 const StoreContext = createContext<IStoreContext>({
-  buyCard: (_, __, ___) => {
+  buyCard: (_) => {
     return new Promise((resolve) => resolve(false));
   },
-  levelUpPlay: (_, __) => {
+  levelUpPlay: (_) => {
     return new Promise((resolve) => resolve(false));
   },
   reroll: () => {
     return new Promise((resolve) => resolve(false));
   },
   locked: false,
+  isPurchased: (_) => false,
 });
 export const useStore = () => useContext(StoreContext);
 
 export const StoreProvider = ({ children }: PropsWithChildren) => {
   const { gameId } = useGameContext();
   const [locked, setLocked] = useState(false);
+  const [purchasedCards, setPurchasedCards] = useState<string[]>([]);
+  const [purchasedPokerHands, setPurchasedPokerHands] = useState<string[]>([]);
+
+  const addPurchasedCard = (card: Card) => {
+    setPurchasedCards((prev) => [...prev, getCardUniqueId(card)]);
+  };
+  const addPokerHandPurchased = (item: PokerHandItem) => {
+    setPurchasedPokerHands((prev) => [...prev, item.poker_hand]);
+  };
+  const isPurchased = (item: Card | PokerHandItem) => {
+    if (item.purchased) {
+      return true;
+    } else if ("poker_hand" in item) {
+      return purchasedPokerHands.includes(item.poker_hand);
+    } else {
+      return purchasedCards.includes(getCardUniqueId(item));
+    }
+  };
+  const clearPurchasedItems = () => {
+    setPurchasedCards([]);
+    setPurchasedPokerHands([]);
+  };
+  const rollbackPurchasedCard = (card: Card) => {
+    setPurchasedCards((prev) =>
+      prev.filter((id) => id !== getCardUniqueId(card))
+    );
+  };
+  const rollbackPokerHandPurchased = (item: PokerHandItem) => {
+    setPurchasedPokerHands((prev) =>
+      prev.filter((id) => id !== item.poker_hand)
+    );
+  };
 
   const {
     setup: {
@@ -42,34 +76,59 @@ export const StoreProvider = ({ children }: PropsWithChildren) => {
     account,
   } = useDojo();
 
-  const buyCard = (
-    card_idx: number,
-    card_type: number,
-    price: number
-  ): Promise<boolean> => {
+  const buyCard = (card: Card): Promise<boolean> => {
     setLocked(true);
-    const promise = dojoBuyCard(account.account, gameId, card_idx, card_type);
-    promise.finally(() => {
-      setLocked(false);
-    });
+    addPurchasedCard(card);
+    const promise = dojoBuyCard(
+      account.account,
+      gameId,
+      card.idx,
+      getCardType(card)
+    );
+    promise
+      .then((response) => {
+        if (!response) {
+          rollbackPurchasedCard(card);
+        }
+      })
+      .catch(() => {
+        rollbackPurchasedCard(card);
+      })
+      .finally(() => {
+        setLocked(false);
+      });
     return promise;
   };
 
   const reroll = () => {
     setLocked(true);
     const promise = storeReroll(account.account, gameId);
-    promise.finally(() => {
-      setLocked(false);
-    });
+    promise
+      .then(() => {
+        clearPurchasedItems();
+      })
+      .finally(() => {
+        setLocked(false);
+      });
     return promise;
   };
 
-  const levelUpPlay = (item_id: number, price: number): Promise<boolean> => {
+  const levelUpPlay = (item: PokerHandItem): Promise<boolean> => {
     setLocked(true);
-    const promise = dojoLevelUpHand(account.account, gameId, item_id);
-    promise.finally(() => {
-      setLocked(false);
-    });
+    addPokerHandPurchased(item);
+    const promise = dojoLevelUpHand(account.account, gameId, item.idx);
+    promise
+      .then((response) => {
+        if (!response) {
+          rollbackPokerHandPurchased(item);
+        }
+      })
+      .catch(() => {
+        rollbackPokerHandPurchased(item);
+      })
+      .finally(() => {
+        setLocked(false);
+      });
     return promise;
   };
 
@@ -80,6 +139,7 @@ export const StoreProvider = ({ children }: PropsWithChildren) => {
         levelUpPlay,
         reroll,
         locked,
+        isPurchased,
       }}
     >
       {children}
