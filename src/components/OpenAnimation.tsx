fc3afaab6ce543f9c46b56351a5a8e16cc1ad1a8
brendamareco <brendaamareco@gmail.com>
2024-08-27 18:53:27 -0300

    Adjusted animation and particles

--- src/components/OpenAnimation.tsx
+++ src/components/OpenAnimation.tsx
@@ -7,10 +7,11 @@ const Particle = styled(motion.div)`
   width: 100%;
   height: 100%;
   background-size: cover;
+  background-repeat: no-repeat;
   background-position: center;
   z-index: 1000;
   mix-blend-mode: screen;
-  filter: hue-rotate(60deg) sepia(1) saturate(5) brightness(1.2);
+  filter: sepia(1) saturate(0) brightness(1);
 `;
 
 interface GlowEffectProps {
@@ -18,13 +19,19 @@ interface GlowEffectProps {
   }
 
 const GlowEffect = styled.div<GlowEffectProps>`
-  filter: ${(props) => (props.glow ? 'drop-shadow(0 0 20px yellow)' : 'none')};
+  filter: ${(props) => (props.glow ? 'drop-shadow(0 0 20px white)' : 'none')};
   transition: filter 0.5s;
+  z-index: 1500;
 `;
 
 const Container = styled.div`
   position: relative; 
   display: inline-block;
+`;
+
+const ChildrenWrapper = styled.div<{ hide: boolean }>`
+  visibility: ${(props) => (props.hide ? 'hidden' : 'visible')};
+  position: relative; 
 `;
 
 interface OpenAnimationProps {
@@ -36,24 +43,28 @@ const OpenAnimation = ({ children }: OpenAnimationProps) => {
   const [shake, setShake] = useState(false);
   const [glow, setGlow] = useState(false);
   const [particle, setParticle] = useState<string | null>(null);
+  const [hideChildren, setHideChildren] = useState(false);
 
   useEffect(() => {
     setShake(true);
-    setParticle('url(/vfx/particle1.gif)');
+    setParticle('url(/vfx/particle2.gif)');
 
     const glowTimeout = setTimeout(() => {
       setShake(false);
       setGlow(true);
-    }, 3000);
+      setParticle('url(/vfx/glow_particle.gif)');
+    }, 500);
 
     const particle2Timeout = setTimeout(() => {
-      setParticle('url(/vfx/explosion.gif)');
-    }, 5000);
+        setHideChildren(true);
+        setParticle('url(/vfx/explosion3.gif)');
+    }, 1500);
 
     const resetTimeout = setTimeout(() => {
-      setGlow(false);
       setParticle(null);
-    }, 7000);
+      setHideChildren(false);
+      setGlow(false);
+    }, 2200);
 
     return () => {
       clearTimeout(glowTimeout);
@@ -72,7 +83,11 @@ const OpenAnimation = ({ children }: OpenAnimationProps) => {
     >
         <Container>
             {particle && <Particle style={{ backgroundImage: particle }} />}
-            <GlowEffect glow={glow}>{children}</GlowEffect>
+            <GlowEffect glow={glow}>
+                <ChildrenWrapper hide={hideChildren}>
+                    {children}
+                </ChildrenWrapper>
+            </GlowEffect>
         </Container>
       
     </motion.div>
