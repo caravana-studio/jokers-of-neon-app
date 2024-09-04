45b27abf7cb014e9d6ff3e423fff720ac0079c83
brendamareco <brendaamareco@gmail.com>
2024-09-04 20:15:51 -0300

    Updated tooltip style

--- src/theme/tooltip.ts
+++ src/theme/tooltip.ts
@@ -1,11 +1,18 @@
-import { background, defineStyleConfig } from "@chakra-ui/react";
+import { cssVar, defineStyleConfig } from "@chakra-ui/react";
+const $arrowBg = cssVar("popper-arrow-bg");
 
 // define the base component styles
 const baseStyle = {
   width: '150px',
   textAlign: 'center',
-  p: 2
+  p: 2,
+  background: 'black',
+  color: 'white',
+  boxShadow: `0px 0px 10px 2px white`,
+  borderRadius: '10px',
+  [$arrowBg.variable]: "colors.black",
 };
+
 
 // export the component theme
 export const tooltipTheme = defineStyleConfig({ baseStyle });
