bf09c6667ac5a0f8dd1a2e4a379a0429f6a0fba9
nicon44 <nicolas@dulio.com.ar>
2024-09-04 15:12:04 -0400

    update ids

--- src/utils/mocks/specialCardMocks.ts
+++ src/utils/mocks/specialCardMocks.ts
@@ -1,23 +1,23 @@
 import { Card } from "../../types/Card";
 
 export const EasyFlush: Card = {
-  img: "/effect/10.png",
-  id: "0",
-  idx: 0,
-  card_id: 10,
+  img: "/effect/309.png",
+  id: "309",
+  idx: 309,
+  card_id: 309,
   isSpecial: true,
 };
 export const EasyStraight: Card = {
-  img: "",
-  id: "0",
-  idx: 0,
-  card_id: 9,
+  img: "/effect/308.png",
+  id: "308",
+  idx: 308,
+  card_id: 308,
   isSpecial: true,
 };
 export const AllCardsToHearts: Card = {
-  img: "",
-  id: "0",
-  idx: 0,
-  card_id: 15,
+  img: "/effect/314.png",
+  id: "314",
+  idx: 314,
+  card_id: 314,
   isSpecial: true,
 };
