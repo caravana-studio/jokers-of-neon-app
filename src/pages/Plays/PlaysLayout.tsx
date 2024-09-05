0dd19c876040d1e4d77166237391677ed8510905
brendamareco <brendaamareco@gmail.com>
2024-09-05 11:13:22 -0300

    Added borders

--- src/pages/Plays/PlaysLayout.tsx
+++ src/pages/Plays/PlaysLayout.tsx
@@ -6,12 +6,21 @@ import {
 import { PlaysTable } from "./PlaysTable";
 import { useNavigate } from "react-router-dom";
 import { Background } from "../../components/Background";
+import { Image } from "@chakra-ui/react";
 
 export const PlaysLayout = () => {
   const navigate = useNavigate();
 
   return (
     <Background type="game" dark>
+      <Image
+            src="/borders/top.png"
+            height="8%"
+            width="100%"
+            maxHeight="70px"
+            position="fixed"
+            top={0}
+          />
       <Flex py={4} px={8} flexDirection={"column"} justifyContent={"center"} width={"50%"} margin={"0 auto"} height={"100vh"}>
         <Heading size="l" color="aqua" textAlign={"center"}>
           AVAILABLE PLAYS
@@ -19,6 +28,15 @@ export const PlaysLayout = () => {
 
         <PlaysTable />
       </Flex>
+      <Image
+            src="/borders/bottom.png"
+            maxHeight="70px"
+            height="8%"
+            width="100%"
+            position="fixed"
+            bottom={0}
+            sx={{ pointerEvents: "none" }}
+          />
     </Background>
   );
 };
