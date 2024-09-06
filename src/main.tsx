57dad48dbe777f93faa7b2959bfaeb1a0fc3ffc4
nicon44 <nicolas@dulio.com.ar>
2024-09-06 15:06:39 -0400

    improve preloading imgs

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import { DojoProvider } from "./dojo/DojoContext.tsx";
 import { setup } from "./dojo/generated/setup.ts";
 import "./index.css";
 import { LoadingScreen } from "./pages/LoadingScreen.tsx";
+import { preloadImages } from "./utils/preloadImages.ts";
 
 async function init() {
   const rootElement = document.getElementById("root");
@@ -17,6 +18,7 @@ async function init() {
   root.render(<LoadingScreen />);
 
   try {
+    preloadImages();
     const setupResult = await setup(dojoConfig);
     const queryClient = new QueryClient();
     root.render(
