8b228b1a0127fb5642f5aa9bca04ea0cd03cb021
Cristian Fleita <cristianagustinfleita@gmail.com>
2024-09-11 12:13:31 -0300

    debug: rollback import

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svgx(), react() , wasm(), topLevelAwait()],
+    plugins: [react() , wasm(), topLevelAwait(), svgx()],
     build: {
       outDir: 'dist',
       target: 'esnext',
