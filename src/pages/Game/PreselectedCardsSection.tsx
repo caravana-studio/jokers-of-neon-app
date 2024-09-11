42d694de29b9517434f1029df8dea9396c8cb996
nicon44 <nicolas@dulio.com.ar>
2024-09-11 17:04:28 -0400

    layout changes

--- src/pages/Game/PreselectedCardsSection.tsx
+++ src/pages/Game/PreselectedCardsSection.tsx
@@ -1,5 +1,4 @@
 import { Box, Flex } from "@chakra-ui/react";
-import { useNavigate } from "react-router-dom";
 import { AnimatedCard } from "../../components/AnimatedCard";
 import { CurrentPlay } from "../../components/CurrentPlay.tsx";
 import { ModifiableCard } from "../../components/ModifiableCard";
@@ -13,7 +12,9 @@ interface PreselectedCardsProps {
   isTutorialRunning?: boolean;
 }
 
-export const PreselectedCardsSection = ({isTutorialRunning = false}: PreselectedCardsProps ) => {
+export const PreselectedCardsSection = ({
+  isTutorialRunning = false,
+}: PreselectedCardsProps) => {
   const {
     preSelectedCards,
     hand,
@@ -24,59 +25,72 @@ export const PreselectedCardsSection = ({isTutorialRunning = false}: Preselected
   } = useGameContext();
 
   return (
-    <>
-    <Box className="game-tutorial-step-3" >
-      <DiscardButton highlight={isTutorialRunning} />
-    </Box>
-
-      <Box
-        sx={{
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
+    <Flex
+      flexDirection={"column"}
+      justifyContent={"space-around"}
+      alignItems={"center"}
+      width={"100%"}
+      height={"100%"}
+    >
+      <Box height='60px'></Box>
+      <Flex
+        flexDirection={"row"}
+        width={"100%"}
+        justifyContent={"center"}
+        alignItems={"center"}
       >
-        <Flex
-          justifyContent="center"
-          alignItems={"center"}
-          height="188px"
-          background={"url(grid.png)"}
-          width="90%"
+        <Box className="game-tutorial-step-3">
+          <DiscardButton highlight={isTutorialRunning} />
+        </Box>
+
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
         >
-          {preSelectedCards.map((idx) => {
-            const card = hand.find((c) => c.idx === idx);
-            const modifiers = getModifiers(idx);
-            const modifiedCard: Card = { ...card!, modifiers };
-            return (
-              card && (
-                <Box key={card.id} mx={{ base: 3, md: 6 }}>
-                  <ModifiableCard id={card.id}>
-                    <AnimatedCard
-                      idx={card.idx}
-                      discarded={discardAnimation}
-                      played={playAnimation}
-                    >
-                      <TiltCard
-                        cursor="pointer"
-                        card={modifiedCard}
-                        onClick={() => {
-                          togglePreselected(idx);
-                        }}
-                      />
-                    </AnimatedCard>
-                  </ModifiableCard>
-                </Box>
-              )
-            );
-          })}
-        </Flex>
-        <CurrentPlay />
-      </Box>
-      <Box className="game-tutorial-step-4" >
-        <PlayButton highlight={isTutorialRunning} />
-      </Box>
-      
-    </>
+          <Flex
+            justifyContent="center"
+            alignItems={"center"}
+            height="188px"
+            background={"url(grid.png)"}
+            width="90%"
+          >
+            {preSelectedCards.map((idx) => {
+              const card = hand.find((c) => c.idx === idx);
+              const modifiers = getModifiers(idx);
+              const modifiedCard: Card = { ...card!, modifiers };
+              return (
+                card && (
+                  <Box key={card.id} mx={{ base: 3, md: 6 }}>
+                    <ModifiableCard id={card.id}>
+                      <AnimatedCard
+                        idx={card.idx}
+                        discarded={discardAnimation}
+                        played={playAnimation}
+                      >
+                        <TiltCard
+                          cursor="pointer"
+                          card={modifiedCard}
+                          onClick={() => {
+                            togglePreselected(idx);
+                          }}
+                        />
+                      </AnimatedCard>
+                    </ModifiableCard>
+                  </Box>
+                )
+              );
+            })}
+          </Flex>
+        </Box>
+        <Box className="game-tutorial-step-4">
+          <PlayButton highlight={isTutorialRunning} />
+        </Box>
+      </Flex>
+      <CurrentPlay />
+    </Flex>
   );
 };
