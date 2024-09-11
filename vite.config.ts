cfc4e0b4aa8de37a1a81054420fcb6fe90914ea5
nicon44 <nicolas@dulio.com.ar>
2024-09-11 12:09:59 -0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import svgx from "@svgx/vite-plugin-react";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
