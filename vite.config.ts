4dd92c942d18f2bc9af8efd5bb1eb31e16f29b88
Cristian Fleita <cristianagustinfleita@gmail.com>
2024-09-11 11:58:43 -0300

    debug: adjust build

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,14 @@ import react from "@vitejs/plugin-react";
 import svgx from "@svgx/vite-plugin-react";
 import topLevelAwait from "vite-plugin-top-level-await";
 
-
-
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [svgx(), react() , wasm(), topLevelAwait()],
     build: {
       outDir: 'dist',
+      target: 'esnext',
+    },
+    esbuild: {
+      jsxInject: `import React from 'react'`,
     },
 });
