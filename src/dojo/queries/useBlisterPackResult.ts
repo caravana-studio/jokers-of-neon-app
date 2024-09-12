9b6c33a7d6f0ac2e56d1f42710d6f2661c3f6a1b
nicon44 <nicolas@dulio.com.ar>
2024-09-12 10:30:04 -0400

    fix bug pick cards from pack

--- src/dojo/queries/useBlisterPackResult.ts
+++ src/dojo/queries/useBlisterPackResult.ts
@@ -1,12 +1,13 @@
 import { useComponentValue } from "@dojoengine/react";
 import { Entity } from "@dojoengine/recs";
 import { getEntityIdFromKeys } from "@dojoengine/utils";
+import { Card } from "../../types/Card";
 import { useDojo } from "../useDojo";
 import { getCardFromCardId } from "../utils/getCardFromCardId";
 import { useGame } from "./useGame";
 
-const sort = (a: number, b: number) => {
-  return a - b;
+const sort = (a: Card, b: Card) => {
+  return (a.card_id ?? 0) - (b.card_id ?? 0);
 };
 
 const getCards = (cardIds: number[]) => {
@@ -28,12 +29,12 @@ export const useBlisterPackResult = () => {
   const entityId = getEntityIdFromKeys([BigInt(gameId)]) as Entity;
 
   const blisterPackResult = useComponentValue(BlisterPackResult, entityId);
-  const cardsArray = (blisterPackResult?.cards ?? [])
-    .map((card) => card && Number((card as any)?.value))
-    .sort(sort);
+  const cardsArray = (blisterPackResult?.cards ?? []).map(
+    (card) => card && Number((card as any)?.value)
+  );
 
   return {
-    cards: getCards(cardsArray ?? []),
+    cards: getCards(cardsArray ?? []).sort(sort),
     cardsPicked: blisterPackResult?.cards_picked ?? false,
   };
 };
