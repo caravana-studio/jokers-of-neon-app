e3cd86ca6495d83f84ba2fca46ac981f1ce05bbb
brendamareco <brendaamareco@gmail.com>
2024-09-19 18:56:23 -0300

    Improved grid and added heading

--- src/pages/Deck/DeckCardsGrid.tsx
+++ src/pages/Deck/DeckCardsGrid.tsx
@@ -2,9 +2,8 @@ import { Box, Flex, Heading } from "@chakra-ui/react";
 import { TiltCard } from "../../components/TiltCard";
 import { CARD_HEIGHT, CARD_WIDTH } from "../../constants/visualProps";
 import { Card } from "../../types/Card";
-import { useEffect, useRef, useState } from "react";
 
-const SCALE = 0.45;
+const SCALE = 0.55;
 const CUSTOM_CARD_WIDTH = CARD_WIDTH * SCALE;
 const CUSTOM_CARD_HEIGHT = CARD_HEIGHT * SCALE;
 
@@ -12,63 +11,23 @@ interface DeckCardsGridProps {
     cards: Card[] | undefined; 
   }
 
-export const DeckCardsGrid: React.FC<DeckCardsGridProps> = ({ cards }) => 
-{
-    const labelRef = useRef(null);
-    const [flexWidth, setFlexWidth] = useState(0);
-    
-    useEffect(() => {
-        const resizeObserver = new ResizeObserver((entries) => {
-        for (let entry of entries) {
-            setFlexWidth(entry.contentRect.width);
-        }
-        });
-        if (labelRef.current) {
-        resizeObserver.observe(labelRef.current);
-        }
-        return () => {
-        if (labelRef.current) {
-            resizeObserver.unobserve(labelRef.current);
-        }
-        };
-    }, []);
-    
-    const calculateCardPosition = (index: number, cards: Card[]) => {
-    
-        if (!cards) return 0;
-    
-        if (cards.length === 1) return flexWidth / 2 - CUSTOM_CARD_WIDTH / 2; 
-        const position = (flexWidth - CUSTOM_CARD_WIDTH) / (cards.length - 1) * index;
-    
-        return position + CUSTOM_CARD_WIDTH / 2;
-    };
-
+  export const DeckCardsGrid: React.FC<DeckCardsGridProps> = ({ cards }) => {
     return (
-        <Box mb={4}>
-            <Flex justifyContent="start" mb={1} id="label" ref={labelRef}>
-                <Heading size="s" color="white" textAlign="center">
-                {/* {suit} */}
-                Deck
-                </Heading>
-            </Flex>
-
-            <Box position="relative" height={CUSTOM_CARD_HEIGHT} id="cards-container">
-                {cards?.map((card, index) => {
-                const cardPosX = calculateCardPosition(index, cards);
-                return (
-                    <Box
-                        key={card.id+String(card.isModifier)}
-                        position="absolute"
-                        left={`${cardPosX}px`}
-                        m={1}
-                        transform={`translateX(-${CUSTOM_CARD_WIDTH / 2}px)`}
-                    >
-                    <TiltCard card={card} scale={SCALE} />
-                    </Box>
-                );
-                })}
-            </Box>
-            
-        </Box>
+      <Box mb={4}>
+        <Flex wrap="wrap" position="relative" w="100%" mb={4}>
+            {cards?.map((card) => (
+                <Box
+                key={card.id + String(card.isModifier)}
+                w={`${CUSTOM_CARD_WIDTH}px`}                                             
+                h={`${CUSTOM_CARD_HEIGHT}px`}                                            
+                position="relative"                                                      
+                mr={`-${CUSTOM_CARD_WIDTH / 5}px`}                                       
+                mb={4}                                                                   
+                >
+                <TiltCard card={card} scale={SCALE} />
+                </Box>
+            ))}
+        </Flex>
+      </Box>
     );
-}
+  };
--- src/pages/Deck/DeckPage.tsx
+++ src/pages/Deck/DeckPage.tsx
@@ -1,6 +1,7 @@
 import { Box, Flex, Heading } from "@chakra-ui/react";
 import { useDeck } from "../../dojo/queries/useDeck";
 import { DeckCardsGrid } from "./DeckCardsGrid";
+import { Background } from "../../components/Background";
 
 export const DeckPage = () => 
     {
@@ -8,11 +9,13 @@ export const DeckPage = () =>
         const cards = deck?.cards;
 
         return(
-            <Box backgroundColor="darkGrey" py={4} px={8} width={"100vw"} height={"100vh"}>
-                <Heading size="lg" color="aqua" textAlign="center">
-                    CURRENT DECK: {cards?.length}
+            <Background type="store">
+                <Flex py={4} px={8} width={"100vw"} height={"100vh"} flexDirection={"column"} justifyContent={"center"}>
+                <Heading variant="italic" size="l" ml={4}>
+                    My full deck
                 </Heading>
-                <DeckCardsGrid cards={deck?.cards}></DeckCardsGrid>
-            </Box>
+                    <DeckCardsGrid cards={deck?.cards}></DeckCardsGrid>
+                </Flex>
+            </Background>
         );
     }
