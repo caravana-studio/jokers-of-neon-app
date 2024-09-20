2aac46a2f0a3756359cc375128def6cf27b58207
Cristian Fleita <cristianagustinfleita@gmail.com>
2024-09-20 20:09:34 -0300

    chore: add share on x btn

--- src/pages/GameOver.tsx
+++ src/pages/GameOver.tsx
@@ -7,6 +7,9 @@ import { GAME_ID, LAST_GAME_ID } from "../constants/localStorage";
 import { getLSGameId } from "../dojo/utils/getLSGameId";
 import { useGameContext } from "../providers/GameProvider";
 import { useGetGame } from "../queries/useGetGame";
+import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useGetLeaderboard } from "../queries/useGetLeaderboard";
 
 const GAME_URL = "https://jokersofneon.com/";
 
@@ -19,7 +22,8 @@ export const GameOver = () => {
   const [lastGameId, setLastGameId] = useState(getLSGameId());
   const { restartGame } = useGameContext();
   const { data } = useGetGame(lastGameId);
-  const score = data?.player_score ?? 0;
+  const { data: fullLeaderboard } = useGetLeaderboard();
+  const actualPlayer = fullLeaderboard?.find((player) => player.id === gameId);
 
   useEffect(() => {
     localStorage.removeItem(GAME_ID);
@@ -40,12 +44,10 @@ export const GameOver = () => {
             GAME OVER
           </Heading>
           <Leaderboard gameId={lastGameId} lines={4} />
+          <Flex mt={8} justifyContent={"space-between"} gap={2}>
           <Button
-            width={"100%"}
+            width={"50%"}
             variant="secondarySolid"
-            sx={{
-              mt: 6,
-            }}
             onClick={() => {
               localStorage.removeItem(GAME_ID);
               restartGame();
@@ -54,7 +56,25 @@ export const GameOver = () => {
           >
             START NEW GAME
           </Button>
+          <Button
+            width={"50%"}
+            variant="solid"
+            onClick={() => {
+              window.open(
+                `https://twitter.com/intent/tweet?text=%F0%9F%83%8F%20I%20just%20finished%20a%20game%20in%20Jokers%20of%20Neon%20%E2%80%94%20check%20out%20my%20results%3A%0A%F0%9F%94%A5%20Level%3A%20${actualPlayer?.level}%0A%F0%9F%8F%85%20Rank%3A%20${actualPlayer?.position}%0A%0AThink%20you%20can%20top%20that%3F%20The%20demo%20is%20live%20for%20a%20limited%20time!%20%E2%8F%B3%0A%0AGive%20it%20a%20try%20at%20https%3A%2F%2Fjokersofneon.com%2F%20%F0%9F%83%8F%E2%9C%A8`,
+                "_blank"
+              );
+            }}
+            data-size="large"
+          >
+            SHARE YOUR SCORE ON
+          <Flex sx={{ ml: 2.5 }}>
+            <FontAwesomeIcon fontSize={22} icon={faXTwitter} />
+          </Flex>
+          </Button>
+          </Flex>
         </Box>
+        
 
         <Image
           position={"fixed"}
