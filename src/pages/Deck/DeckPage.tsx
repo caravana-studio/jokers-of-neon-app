6a51b717bf99a98a119429c72c34fca4eaa648c9
brendamareco <brendaamareco@gmail.com>
2024-09-19 23:46:00 -0300

    Updated layout

--- src/pages/Deck/DeckPage.tsx
+++ src/pages/Deck/DeckPage.tsx
@@ -1,7 +1,8 @@
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react";
 import { useDeck } from "../../dojo/queries/useDeck";
 import { DeckCardsGrid } from "./DeckCardsGrid";
 import { Background } from "../../components/Background";
+import { BLUE_LIGHT } from "../../theme/colors";
 
 export const DeckPage = () => 
     {
@@ -10,11 +11,87 @@ export const DeckPage = () =>
 
         return(
             <Background type="store">
-                <Flex py={4} px={8} width={"100vw"} height={"100vh"} flexDirection={"column"} justifyContent={"center"}>
-                <Heading variant="italic" size="l" ml={4}>
-                    My full deck
-                </Heading>
-                    <DeckCardsGrid cards={deck?.cards}></DeckCardsGrid>
+                <Flex py={4} px={20} width={"100vw"} height={"100vh"} alignItems={"center"} justifyContent={"center"} gap={12}>
+                    <Flex alignItems={"center"}  width={"40%"} height={"50%"} flexDirection={"column"}>
+                    
+                    <Heading 
+                        variant="italic" 
+                        size="l"
+                        width={"100%"}
+                        ml={4}
+                        textAlign={"center"}
+                        sx={{
+                            position: "relative",
+                            _after: {
+                                content: '""',
+                                position: "absolute",
+                                top: "-12px",
+                                left: 0,
+                                width: "100%",
+                                height: "1px",
+                                background:
+                                    `linear-gradient(to right, rgba(255, 255, 255, 0) 0%, ${BLUE_LIGHT} 50%, rgba(255, 255, 255, 0) 100%)`,
+                                boxShadow:
+                                    "0 0 10px rgba(255, 255, 255, 0.5), 0 0 10px rgba(255, 255, 255, 0.5)",
+                            },
+                            _before: {
+                                content: '""',
+                                position: "absolute",
+                                bottom: "-12px",
+                                left: 0,
+                                width: "100%",
+                                height: "1px",
+                                background:
+                                    `linear-gradient(to right, rgba(255, 255, 255, 0) 0%, ${BLUE_LIGHT} 50%, rgba(255, 255, 255, 0) 100%)`,
+                                boxShadow:
+                                    "0 0 10px rgba(255, 255, 255, 0.5), 0 0 10px rgba(255, 255, 255, 0.5)",
+                            },
+                        }}
+                    >
+                        My full deck
+                    </Heading>
+                    <Flex flexDirection={"column"} my={12} alignItems={"center"}>
+                        <Text 
+                            size={"sm"}
+                            sx={{
+                                position: "relative",
+                                _before: {
+                                    content: '""',
+                                    position: "absolute",
+                                    bottom: "0px",
+                                    left: 0,
+                                    width: "100%",
+                                    height: "1px",
+                                    background:
+                                        `linear-gradient(to right, rgba(255, 255, 255, 0.5) 0%, rgba(255, 255, 255, 1) 50%, rgba(255, 255, 255, 0.5) 100%)`,
+                                    boxShadow:
+                                        "0 0 10px rgba(255, 255, 255, 0.5), 0 0 10px rgba(255, 255, 255, 0.5)",
+                                },
+                            }}
+                        >
+                            Filter by
+                        </Text>
+                        <Flex alignItems={"space-around"} justifyContent={"center"} wrap={"wrap"} gap={8} mt={8}>
+                            <Button size={"sm"}>CLUBS</Button>
+                            <Button size={"sm"}>SPADE</Button>
+                            <Button size={"sm"}>HEART</Button>
+                            <Button size={"sm"}>DIAMOND</Button>
+                            <Button size={"sm"}>NEON</Button>
+                            <Button size={"sm"}>MODIFIER</Button>
+                        </Flex>
+                    </Flex>
+
+                    <Flex gap={4} mt={20}>
+                        <Button>SEE SPECIAL CARDS</Button>
+                        <Button>BACK TO GAME</Button>
+                    </Flex>
+
+                    </Flex>
+                    <Flex alignItems={"center"} width={"60%"} height={"60%"} overflowY="auto">
+                    <Box w="100%" h="100%"> 
+                        <DeckCardsGrid cards={deck?.cards} />
+                    </Box>
+                    </Flex>
                 </Flex>
             </Background>
         );
