a04fb54b248bb48d18297d839c3027b1be54a39e
nicon44 <nicolas@dulio.com.ar>
2024-09-27 14:27:18 -0400

    remove logs

--- src/dojo/typescript/contracts.gen.ts
+++ src/dojo/typescript/contracts.gen.ts
@@ -54,8 +54,6 @@ export async function setupWorld(provider: DojoProvider) {
       player_name: bigint;
     }) => {
       try {
-        console.log("player: ", props.player_name, "contract_name: ", contract_name);
-        console.log("provider: ", provider, "acc: ", props.account);
         return await provider.execute(
           props.account,
           {
