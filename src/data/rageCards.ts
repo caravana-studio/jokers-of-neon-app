297d7e534c3b2b6f73e64e18685887031a8538e8
Cristian Fleita <cristianagustinfleita@gmail.com>
2024-10-01 18:48:07 -0300

    chore: WIP rage card description

--- src/data/rageCards.ts
+++ src/data/rageCards.ts
@@ -1,40 +1,45 @@
 import { CardDataMap } from "../types/CardData";
+import i18n from 'i18next';
 
-export const RAGE_CARDS_DATA: CardDataMap = {
-  401: {
-    name: "Broken Hearts",
-    description: "Heart suited cards will not score during this level",
-  },
-  402: {
-    name: "Broken Clubs",
-    description: "Club suited cards will not score during this level",
-  },
-  403: {
-    name: "Broken Diamonds",
-    description: "Diamond suited cards will not score during this level",
-  },
-  404: {
-    name: "Broken Spades",
-    description: "Spade suited cards will not score during this level",
-  },
-  405: {
-    name: "Broken Jokers",
-    description: "Jokers will not score during this level",
-  },
-  406: {
-    name: "Diminished Hold",
-    description: "Your hand will be reduced by 2 cards during this level",
-  },
-  407: {
-    name: "Zero Waste",
-    description: "You have no discards during this level",
-  },
-  408: {
-    name: "Unexpected Sacrifice",
-    description: "One of your special cards will be sacrificed",
-  },
-  409: {
-    name: "Strategic Quartet",
-    description: "You can only play up to 4 cards during this level",
-  },
-};
+export const RAGE_CARDS_DATA: CardDataMap = {}
+
+i18n.on('initialized', () => {
+  Object.assign(RAGE_CARDS_DATA, {
+    401: {
+      name: i18n.t('rageCardsData.401.name'),
+      description: i18n.t('rageCardsData.401.description'),
+    },
+    402: {
+      name: i18n.t('rageCardsData.402.name'),
+      description: i18n.t('rageCardsData.402.description'),
+    },
+    403: {
+      name: i18n.t('rageCardsData.403.name'),
+      description: i18n.t('rageCardsData.403.description'),
+    },
+    404: {
+      name: i18n.t('rageCardsData.404.name'),
+      description: i18n.t('rageCardsData.404.description'),
+    },
+    405: {
+      name: i18n.t('rageCardsData.405.name'),
+      description: i18n.t('rageCardsData.405.description'),
+    },
+    406: {
+      name: i18n.t('rageCardsData.406.name'),
+      description: i18n.t('rageCardsData.406.description'),
+    },
+    407: {
+      name: i18n.t('rageCardsData.407.name'),
+      description: i18n.t('rageCardsData.407.description'),
+    },
+    408: {
+      name: i18n.t('rageCardsData.408.name'),
+      description: i18n.t('rageCardsData.408.description'),
+    },
+    409: {
+      name: i18n.t('rageCardsData.409.name'),
+      description: i18n.t('rageCardsData.409.description'),
+    }
+  });
+});
