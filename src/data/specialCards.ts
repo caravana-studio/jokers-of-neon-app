096913b40bc528f0bc40cb2449fd841875c801ea
Cristian Fleita <cristianagustinfleita@gmail.com>
2024-10-01 18:54:46 -0300

    chore: WIP special cards descriptions

--- src/data/specialCards.ts
+++ src/data/specialCards.ts
@@ -1,90 +1,91 @@
 import { CardDataMap } from "../types/CardData";
+import i18n from 'i18next';
 
-export const SPECIAL_CARDS_DATA: CardDataMap = {
-  300: {
-    name: "Multiplied Hearts",
-    description: "Adds +2 multi for every played Heart suited card",
-  },
-  301: {
-    name: "Multiplied Clubs",
-    description: "Adds +2 multi for every played Clubs suited card",
-  },
-  302: {
-    name: "Multiplied Diamonds",
-    description: "Adds +2 multi for every played Diamonds suited card",
-  },
-  303: {
-    name: "Multiplied Spades",
-    description: "Adds +2 multi for every played Spades suited card",
-  },
-  304: {
-    name: "Pair Booster",
-    description: "Level up PAIR hand by 4 levels",
-  },
-  305: {
-    name: "Two Pair Booster",
-    description: "Level up TWO PAIR hand by 4 levels",
-  },
-  306: {
-    name: "Straight Booster",
-    description: "Level up STRAIGHT hand by 4 levels",
-  },
-  307: {
-    name: "Flush Booster",
-    description: "Level up FLUSH hand by 4 levels",
-  },
-  308: {
-    name: "Easy Straight",
-    description: "Straight hand can be done with 4 cards",
-  },
-  309: {
-    name: "Easy Flush",
-    description: "Flush hand can be done with 4 cards",
-  },
-  310: {
-    name: "Joker Booster",
-    description: "All played Jokers score double points and double multi",
-  },
-  311: {
-    name: "Modifier Booster",
-    description:
-      "All played MODIFIER cards score double points and double multi",
-  },
-  312: {
-    name: "Figures Booster",
-    description: "All played FIGURES cards score +100 points",
-  },
-  313: {
-    name: "Multiplied Aces",
-    description: "Adds +3 multi for every played Ace card",
-  },
-  314: {
-    name: "Love is in the air",
-    description: "All played cards are considered Heart suited",
-  },
-  315: {
-    name: "The hand thief",
-    description: "Adds +1 hands and +1 discards to the round",
-  },
-  316: {
-    name: "Extra help",
-    description: "Increase hand size by 2",
-  },
-  317: {
-    name: "Lucky 7",
-    description: "All 7s score +7 multi",
-  },
-  318: {
-    name: "Neon bonus",
-    description: "All Neon cards score +20 points",
-  },
-  319: {
-    name: "Deadline",
-    description:
-      "When playing your last hand, increase the play level 10 times",
-  },
-  320: {
-    name: "Initial advantage",
-    description: "The first played hand scores +100 points and +10 multi",
-  },
-};
+export const SPECIAL_CARDS_DATA: CardDataMap = {};
+i18n.on('initialized', () => {
+  Object.assign(SPECIAL_CARDS_DATA, {
+    300: {
+      name: i18n.t('specialCardsData.300.name'),
+      description: i18n.t('specialCardsData.300.description'),
+    },
+    301: {
+      name: i18n.t('specialCardsData.301.name'),
+      description: i18n.t('specialCardsData.301.description'),
+    },
+    302: {
+      name: i18n.t('specialCardsData.302.name'),
+      description: i18n.t('specialCardsData.302.description'),
+    },
+    303: {
+      name: i18n.t('specialCardsData.303.name'),
+      description: i18n.t('specialCardsData.303.description'),
+    },
+    304: {
+      name: i18n.t('specialCardsData.304.name'),
+      description: i18n.t('specialCardsData.304.description'),
+    },
+    305: {
+      name: i18n.t('specialCardsData.305.name'),
+      description: i18n.t('specialCardsData.305.description'),
+    },
+    306: {
+      name: i18n.t('specialCardsData.306.name'),
+      description: i18n.t('specialCardsData.306.description'),
+    },
+    307: {
+      name: i18n.t('specialCardsData.307.name'),
+      description: i18n.t('specialCardsData.307.description'),
+    },
+    308: {
+      name: i18n.t('specialCardsData.308.name'),
+      description: i18n.t('specialCardsData.308.description'),
+    },
+    309: {
+      name: i18n.t('specialCardsData.309.name'),
+      description: i18n.t('specialCardsData.309.description'),
+    },
+    310: {
+      name: i18n.t('specialCardsData.310.name'),
+      description: i18n.t('specialCardsData.310.description'),
+    },
+    311: {
+      name: i18n.t('specialCardsData.311.name'),
+      description: i18n.t('specialCardsData.311.description'),
+    },
+    312: {
+      name: i18n.t('specialCardsData.312.name'),
+      description: i18n.t('specialCardsData.312.description'),
+    },
+    313: {
+      name: i18n.t('specialCardsData.313.name'),
+      description: i18n.t('specialCardsData.313.description'),
+    },
+    314: {
+      name: i18n.t('specialCardsData.314.name'),
+      description: i18n.t('specialCardsData.314.description'),
+    },
+    315: {
+      name: i18n.t('specialCardsData.315.name'),
+      description: i18n.t('specialCardsData.315.description'),
+    },
+    316: {
+      name: i18n.t('specialCardsData.316.name'),
+      description: i18n.t('specialCardsData.316.description'),
+    },
+    317: {
+      name: i18n.t('specialCardsData.317.name'),
+      description: i18n.t('specialCardsData.317.description'),
+    },
+    318: {
+      name: i18n.t('specialCardsData.318.name'),
+      description: i18n.t('specialCardsData.318.description'),
+    },
+    319: {
+      name: i18n.t('specialCardsData.319.name'),
+      description: i18n.t('specialCardsData.319.description'),
+    },
+    320: {
+      name: i18n.t('specialCardsData.320.name'),
+      description: i18n.t('specialCardsData.320.description'),
+    }});
+});
