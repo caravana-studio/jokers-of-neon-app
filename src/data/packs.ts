4050bb2086ffd48d6a879db075b5ed0ed77ca51b
Cristian Fleita <cristianagustinfleita@gmail.com>
2024-10-04 12:20:19 -0300

    refactor: packs data

--- src/data/packs.ts
+++ src/data/packs.ts
@@ -3,52 +3,58 @@ import i18n from 'i18next';
 
 export const PACKS_DATA: CardDataMap = {};
 
-i18n.on('initialized', () => {
+const loadTranslations = async () => {
+  await i18n.loadNamespaces(['packs']);
+
   Object.assign(PACKS_DATA, {
     1: {
-      name: i18n.t('packsData.1.name'),
-      description: i18n.t('packsData.1.description'),
-      details: i18n.t('packsData.1.details'),
+      name: i18n.t('packsData.1.name', {ns:'packs'}),
+      description: i18n.t('packsData.1.description', {ns:'packs'}),
+      details: i18n.t('packsData.1.details', {ns:'packs'}),
     },
     2: {
-      name: i18n.t('packsData.2.name'),
-      description: i18n.t('packsData.2.description'),
-      details: i18n.t('packsData.2.details'),
+      name: i18n.t('packsData.2.name', {ns:'packs'}),
+      description: i18n.t('packsData.2.description', {ns:'packs'}),
+      details: i18n.t('packsData.2.details', {ns:'packs'}),
     },
     3: {
-      name: i18n.t('packsData.3.name'),
-      description: i18n.t('packsData.3.description'),
-      details: i18n.t('packsData.3.details'),
+      name: i18n.t('packsData.3.name', {ns:'packs'}),
+      description: i18n.t('packsData.3.description', {ns:'packs'}),
+      details: i18n.t('packsData.3.details', {ns:'packs'}),
     },
     4: {
-      name: i18n.t('packsData.4.name'),
-      description: i18n.t('packsData.4.description'),
-      details: i18n.t('packsData.4.details'),
+      name: i18n.t('packsData.4.name', {ns:'packs'}),
+      description: i18n.t('packsData.4.description', {ns:'packs'}),
+      details: i18n.t('packsData.4.details', {ns:'packs'}),
     },
     5: {
-      name: i18n.t('packsData.5.name'),
-      description: i18n.t('packsData.5.description'),
-      details: i18n.t('packsData.5.details'),
+      name: i18n.t('packsData.5.name', {ns:'packs'}),
+      description: i18n.t('packsData.5.description', {ns:'packs'}),
+      details: i18n.t('packsData.5.details', {ns:'packs'}),
     },
     6: {
-      name: i18n.t('packsData.6.name'),
-      description: i18n.t('packsData.6.description'),
-      details: i18n.t('packsData.6.details'),
+      name: i18n.t('packsData.6.name', {ns:'packs'}),
+      description: i18n.t('packsData.6.description', {ns:'packs'}),
+      details: i18n.t('packsData.6.details', {ns:'packs'}),
     },
     7: {
-      name: i18n.t('packsData.7.name'),
-      description: i18n.t('packsData.7.description'),
-      details: i18n.t('packsData.7.details'),
+      name: i18n.t('packsData.7.name', {ns:'packs'}),
+      description: i18n.t('packsData.7.description', {ns:'packs'}),
+      details: i18n.t('packsData.7.details', {ns:'packs'}),
     },
     8: {
-      name: i18n.t('packsData.8.name'),
-      description: i18n.t('packsData.8.description'),
-      details: i18n.t('packsData.8.details'),
+      name: i18n.t('packsData.8.name', {ns:'packs'}),
+      description: i18n.t('packsData.8.description', {ns:'packs'}),
+      details: i18n.t('packsData.8.details', {ns:'packs'}),
     },
     9: {
-      name: i18n.t('packsData.9.name'),
-      description: i18n.t('packsData.9.description'),
-      details: i18n.t('packsData.9.details'),
+      name: i18n.t('packsData.9.name', {ns:'packs'}),
+      description: i18n.t('packsData.9.description', {ns:'packs'}),
+      details: i18n.t('packsData.9.details', {ns:'packs'}),
     },
   });  
-});
+
+  console.log("Translations loaded", PACKS_DATA);
+};
+
+i18n.on('initialized', loadTranslations);
