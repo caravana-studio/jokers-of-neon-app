ec8c30e87184f89727ff74b3eb975a02ecab732f
dub_zn <santiagodgalvan@gmail.com>
2024-10-04 00:26:53 -0300

    revert usePokerPlays

--- src/dojo/queries/usePokerPlays.tsx
+++ src/dojo/queries/usePokerPlays.tsx
@@ -1,14 +1,15 @@
 import {
-    getComponentValue,
-    Has,
-  } from "@dojoengine/recs";
+  getComponentValue,
+  Has,
+} from "@dojoengine/recs";
 import { PlayerLevelPokerHand } from "../typescript/models.gen";
 import { useEffect, useState } from "react";
 import { useDojo } from "../useDojo";
 import { useEntityQuery } from "@dojoengine/react";
+
 export const usePokerPlays = (gameId: number): { plays: PlayerLevelPokerHand[] } => {
   const [plays, setPlays] = useState<any>({});
-
+  
   const {
       setup: {
           clientComponents: {
@@ -16,20 +17,21 @@ export const usePokerPlays = (gameId: number): { plays: PlayerLevelPokerHand[] }
         },
       },
     } = useDojo();
-
+  
     const gameKeys = useEntityQuery([Has(PlayerLevelPokerHand)]);
-
+  
     useEffect(() => {
       const components = gameKeys.map((entity) => {
         const component = getComponentValue(PlayerLevelPokerHand, entity);
-
+  
         if (!component) {
           return undefined;
         }
         return component;
       }).filter((component) => component?.game_id === gameId);;
-
+   
       setPlays(components);
     }, [gameKeys]);
+  
   return {plays: Object.values(plays)};
 }
