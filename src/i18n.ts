af7e4cc2640cb9d5eff28a7799a06f935ea49d80
Cristian Fleita <cristianagustinfleita@gmail.com>
2024-10-04 21:05:07 -0300

    refactor: disable debug i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,20 +3,18 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-// Initialize i18n with TypeScript support
 i18n
-  .use(Backend) // Load translations from the server
+  .use(Backend)
   .use(LanguageDetector) // Detect the user's language
   .use(initReactI18next) // Bind i18next to React
   .init({
-    fallbackLng: 'en', // Fallback language if the user's language cannot be detected
-    debug: true, // Enable debug mode for development
+    fallbackLng: 'en',
+    debug: false, 
     interpolation: {
       escapeValue: false, // React already escapes values to prevent XSS, so disable escaping
     },
     backend: {
       loadPath: function(lngs: string[], namespaces: string[] ) {
-        // Return an array of paths, allowing the loading of multiple files in folder structure
         return namespaces.map(ns => `/locales/${lngs}/${ns}/translation.json`);
       }
     },
