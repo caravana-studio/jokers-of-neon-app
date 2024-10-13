2ccd5c72577cb8b5d50e774f963425fcc8ea5484
brendamareco <brendaamareco@gmail.com>
2024-10-13 20:28:05 -0300

    added function to calculate card scale

--- src/theme/responsiveSettings.tsx
+++ src/theme/responsiveSettings.tsx
@@ -5,29 +5,37 @@ export const useResponsiveValues = () => {
   const [aspectRatio, setAspectRatio] = useState<number>(
     window.innerWidth / window.innerHeight
   );
+  const [baseScale, setBaseScale] = useState<number | undefined>(undefined);
+
+  const defaultBaseScale = useBreakpointValue({
+    base: 0.25,
+    sm: 0.74,
+    md: 0.65,
+  });
 
   useEffect(() => {
     const handleResize = () => {
       const newAspectRatio = window.innerWidth / window.innerHeight;
       setAspectRatio(newAspectRatio);
+
+      if (window.innerWidth === 540 && window.innerHeight === 720) {
+        setBaseScale(0.4); // Adjust for 540x720 resolution
+      } else if (window.innerWidth === 1024 && window.innerHeight === 1366) {
+        setBaseScale(0.9); // Adjust for 1024x1366 resolution
+      } else {
+        setBaseScale(defaultBaseScale);
+      }
     };
 
+    handleResize();
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  }, [defaultBaseScale]);
 
-  const baseScale = useBreakpointValue({
-    base: 0.25,
-    sm: 0.78,
-    md: 0.45,
-  });
-
-  const cardScale =
-    baseScale && aspectRatio
-      ? aspectRatio > 1
-        ? baseScale * aspectRatio
-        : baseScale * (1 / aspectRatio)
-      : 1;
+  const cardScale = baseScale
+    ? baseScale *
+      (aspectRatio > 1 ? 1 + (aspectRatio - 1) * 0.5 : 1 / aspectRatio)
+    : 1;
 
   const isSmallScreen = useBreakpointValue({ base: true, md: false });
 
