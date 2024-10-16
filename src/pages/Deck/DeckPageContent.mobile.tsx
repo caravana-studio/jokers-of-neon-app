5cff7abd300a5a7fe00541f2767b0b778a004a45
Cristian Fleita <cristianagustinfleita@gmail.com>
2024-10-16 20:02:37 -0300

    chore: apply changes to mobile

--- src/pages/Deck/DeckPageContent.mobile.tsx
+++ src/pages/Deck/DeckPageContent.mobile.tsx
@@ -6,181 +6,270 @@ import { useState } from "react";
 import { Suits } from "../../enums/suits";
 import { useNavigate } from "react-router-dom";
 import { CurrentSpecialCardsModal } from "../../components/CurrentSpecialCardsModal";
-import { DeckFiltersMap, createUsedCardsList, preprocessCards } from "./Utils/DeckCardsUtils";
+import {
+  DeckFiltersMap,
+  createUsedCardsList,
+  preprocessCards,
+} from "./Utils/DeckCardsUtils";
+import { useGame } from "../../dojo/queries/useGame";
+import { useGetDeck } from "../../queries/useGetDeck";
+import { useTranslation } from "react-i18next";
 
-export const DeckPageContentMobile = () => 
-    {
-        const fullDeck = preprocessCards(useFullDeck()?.cards ?? []);
-        const currentDeck = preprocessCards(useCurrentDeck()?.cards ?? []);
-        const usedCards = createUsedCardsList(fullDeck ?? [], currentDeck ?? []);
+export const DeckPageContentMobile = () => {
+  const game = useGame();
+  const { t } = useTranslation(["game"]);
 
-        const navigate = useNavigate();
-        const [specialCardsModalOpen, setSpecialCardsModalOpen] = useState(false);
-        const [filterButtonsState, setFilterButtonsState] = useState<DeckFiltersMap>({
-            isNeon: undefined,
-            isModifier: undefined,
-            suit: null,
-          });
+  const fullDeck = preprocessCards(useFullDeck()?.cards ?? []);
+  const currentDeck = preprocessCards(useGetDeck(game?.id ?? 0).cards ?? []);
+  const usedCards = createUsedCardsList(fullDeck ?? [], currentDeck ?? []);
 
-        const updateFilters = (newFilters: DeckCardsFilters) =>
-        {
-            setFilterButtonsState((prevState) => ({
-                isNeon: newFilters.isNeon !== undefined ? newFilters.isNeon : undefined,
-                isModifier: newFilters.isModifier !== undefined ? newFilters.isModifier : undefined,
-                suit: newFilters.suit !== undefined ? newFilters.suit : null,
-              }));
-        };
+  const navigate = useNavigate();
+  const [specialCardsModalOpen, setSpecialCardsModalOpen] = useState(false);
+  const [filterButtonsState, setFilterButtonsState] = useState<DeckFiltersMap>({
+    isNeon: undefined,
+    isModifier: undefined,
+    suit: null,
+  });
 
-        return(
-            <>
-                {specialCardsModalOpen && (
-                    <CurrentSpecialCardsModal
-                    close={() => setSpecialCardsModalOpen(false)}
-                    />
-                )}
-                <Flex py={4} px={{base: 8, md: 20}} width={"100vw"} height={"100vh"} alignItems={"center"} justifyContent={"center"} gap={12} flexDirection={"column"}>
-                    <Flex alignItems={"center"}  width={"100%"} flexDirection={"column"}>
-                        <Heading 
-                            variant="italic" 
-                            size="l"
-                            width={"100%"}
-                            ml={4}
-                            textAlign={"center"}
-                            sx={{
-                                position: "relative",
-                                _after: {
-                                    content: '""',
-                                    position: "absolute",
-                                    top: "-12px",
-                                    left: 0,
-                                    width: "100%",
-                                    height: "1px",
-                                    background:
-                                        `linear-gradient(to right, rgba(255, 255, 255, 0) 0%, ${BLUE_LIGHT} 50%, rgba(255, 255, 255, 0) 100%)`,
-                                    boxShadow:
-                                        "0 0 10px rgba(255, 255, 255, 0.5), 0 0 10px rgba(255, 255, 255, 0.5)",
-                                },
-                                _before: {
-                                    content: '""',
-                                    position: "absolute",
-                                    bottom: "-12px",
-                                    left: 0,
-                                    width: "100%",
-                                    height: "1px",
-                                    background:
-                                        `linear-gradient(to right, rgba(255, 255, 255, 0) 0%, ${BLUE_LIGHT} 50%, rgba(255, 255, 255, 0) 100%)`,
-                                    boxShadow:
-                                        "0 0 10px rgba(255, 255, 255, 0.5), 0 0 10px rgba(255, 255, 255, 0.5)",
-                                },
-                            }}
-                        >
-                            My full deck
-                        </Heading>
-                        <Flex flexDirection={"column"} my={2} alignItems={"center"}>
-                            <Text 
-                                size={"sm"}
-                                sx={{
-                                    position: "relative",
-                                    _before: {
-                                        content: '""',
-                                        position: "absolute",
-                                        bottom: "0px",
-                                        left: 0,
-                                        width: "100%",
-                                        height: "1px",
-                                        background:
-                                            `linear-gradient(to right, rgba(255, 255, 255, 0.5) 0%, rgba(255, 255, 255, 1) 50%, rgba(255, 255, 255, 0.5) 100%)`,
-                                        boxShadow:
-                                            "0 0 10px rgba(255, 255, 255, 0.5), 0 0 10px rgba(255, 255, 255, 0.5)",
-                                    },
-                                }}
-                            >
-                                Filter by
-                            </Text>
-                            <Flex alignItems={"space-around"} justifyContent={"center"} wrap={"wrap"} gap={4} mt={8} width={"95%"}>
-                            <Button
-                                    size={"sm"}
-                                    variant={ filterButtonsState.suit === Suits.CLUBS ? "outlineSecondaryGlowActive" : "outlineSecondaryGlow"}
-                                    borderRadius={"25px"}
-                                    onClick={() => updateFilters({suit: filterButtonsState.suit !== Suits.CLUBS ? Suits.CLUBS : undefined})}
-                                >
-                                    CLUB
-                                </Button>
-                                <Button
-                                    size={"sm"}
-                                    variant={ filterButtonsState.suit === Suits.SPADES ? "outlineSecondaryGlowActive" : "outlineSecondaryGlow"}
-                                    borderRadius={"25px"}
-                                    onClick={() => updateFilters({suit: filterButtonsState.suit !== Suits.SPADES ? Suits.SPADES : undefined})}
-                                >
-                                    SPADE
-                                </Button>
-                                <Button
-                                    size={"sm"}
-                                    variant={ filterButtonsState.suit === Suits.HEARTS ? "outlineSecondaryGlowActive" : "outlineSecondaryGlow"}
-                                    borderRadius={"25px"}
-                                    onClick={() => updateFilters({suit: filterButtonsState.suit !== Suits.HEARTS ? Suits.HEARTS : undefined})}
-                                >
-                                    HEART
-                                </Button>
-                                <Button
-                                    size={"sm"}
-                                    variant={ filterButtonsState.suit === Suits.DIAMONDS ? "outlineSecondaryGlowActive" : "outlineSecondaryGlow"}
-                                    borderRadius={"25px"}
-                                    onClick={() => updateFilters({suit: filterButtonsState.suit !== Suits.DIAMONDS ? Suits.DIAMONDS : undefined})}
-                                >
-                                    DIAMOND
-                                </Button>
-                                <Button
-                                    size={"sm"}
-                                    variant={ filterButtonsState.isNeon ? "outlineSecondaryGlowActive" : "outlineSecondaryGlow"}
-                                    borderRadius={"25px"}
-                                    onClick={() => updateFilters({isNeon: !filterButtonsState.isNeon ? true : undefined})}
-                                >
-                                    NEON
-                                </Button>
-                                <Button
-                                    size={"sm"}
-                                    variant={ filterButtonsState.isModifier ? "outlineSecondaryGlowActive" : "outlineSecondaryGlow"}
-                                    borderRadius={"25px"}
-                                    onClick={() => updateFilters({isModifier: !filterButtonsState.isModifier ? true : undefined})}
-                                >
-                                    MODIFIER
-                                </Button>
-                            </Flex>
-                        </Flex>
-                    </Flex>
+  const updateFilters = (newFilters: DeckCardsFilters) => {
+    setFilterButtonsState((prevState) => ({
+      isNeon: newFilters.isNeon !== undefined ? newFilters.isNeon : undefined,
+      isModifier:
+        newFilters.isModifier !== undefined ? newFilters.isModifier : undefined,
+      suit: newFilters.suit !== undefined ? newFilters.suit : null,
+    }));
+  };
 
-                    <Flex alignItems={"center"} width={"95%"} height={"60%"} overflowY="auto">
-                        <Box w="100%" h="100%"> 
-                            <DeckCardsGrid
-                                cards={fullDeck}
-                                usedCards={usedCards}
-                                filters={{
-                                    isNeon: filterButtonsState.isNeon,
-                                    isModifier: filterButtonsState.isModifier,
-                                    suit: filterButtonsState.suit ?? undefined
-                                 }} 
-                            />
-                        </Box>
-                    </Flex>
+  return (
+    <>
+      {specialCardsModalOpen && (
+        <CurrentSpecialCardsModal
+          close={() => setSpecialCardsModalOpen(false)}
+        />
+      )}
+      <Flex
+        py={4}
+        px={{ base: 8, md: 20 }}
+        width={"100vw"}
+        height={"100vh"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        gap={12}
+        flexDirection={"column"}
+      >
+        <Flex alignItems={"center"} width={"100%"} flexDirection={"column"}>
+          <Heading
+            variant="italic"
+            size="l"
+            width={"100%"}
+            ml={4}
+            textAlign={"center"}
+            sx={{
+              position: "relative",
+              _after: {
+                content: '""',
+                position: "absolute",
+                top: "-12px",
+                left: 0,
+                width: "100%",
+                height: "1px",
+                background: `linear-gradient(to right, rgba(255, 255, 255, 0) 0%, ${BLUE_LIGHT} 50%, rgba(255, 255, 255, 0) 100%)`,
+                boxShadow:
+                  "0 0 10px rgba(255, 255, 255, 0.5), 0 0 10px rgba(255, 255, 255, 0.5)",
+              },
+              _before: {
+                content: '""',
+                position: "absolute",
+                bottom: "-12px",
+                left: 0,
+                width: "100%",
+                height: "1px",
+                background: `linear-gradient(to right, rgba(255, 255, 255, 0) 0%, ${BLUE_LIGHT} 50%, rgba(255, 255, 255, 0) 100%)`,
+                boxShadow:
+                  "0 0 10px rgba(255, 255, 255, 0.5), 0 0 10px rgba(255, 255, 255, 0.5)",
+              },
+            }}
+          >
+            {t("game.deck.title")}
+          </Heading>
+          <Flex flexDirection={"column"} my={2} alignItems={"center"}>
+            <Text
+              size={"sm"}
+              sx={{
+                position: "relative",
+                _before: {
+                  content: '""',
+                  position: "absolute",
+                  bottom: "0px",
+                  left: 0,
+                  width: "100%",
+                  height: "1px",
+                  background: `linear-gradient(to right, rgba(255, 255, 255, 0.5) 0%, rgba(255, 255, 255, 1) 50%, rgba(255, 255, 255, 0.5) 100%)`,
+                  boxShadow:
+                    "0 0 10px rgba(255, 255, 255, 0.5), 0 0 10px rgba(255, 255, 255, 0.5)",
+                },
+              }}
+            >
+              {t("game.deck.filter-title")}
+            </Text>
+            <Flex
+              alignItems={"space-around"}
+              justifyContent={"center"}
+              wrap={"wrap"}
+              gap={4}
+              mt={8}
+              width={"95%"}
+            >
+              <Button
+                size={"sm"}
+                variant={
+                  filterButtonsState.suit === Suits.CLUBS
+                    ? "outlineSecondaryGlowActive"
+                    : "outlineSecondaryGlow"
+                }
+                borderRadius={"25px"}
+                onClick={() =>
+                  updateFilters({
+                    suit:
+                      filterButtonsState.suit !== Suits.CLUBS
+                        ? Suits.CLUBS
+                        : undefined,
+                  })
+                }
+              >
+                {t("game.deck.suit.club").toUpperCase()}
+              </Button>
+              <Button
+                size={"sm"}
+                variant={
+                  filterButtonsState.suit === Suits.SPADES
+                    ? "outlineSecondaryGlowActive"
+                    : "outlineSecondaryGlow"
+                }
+                borderRadius={"25px"}
+                onClick={() =>
+                  updateFilters({
+                    suit:
+                      filterButtonsState.suit !== Suits.SPADES
+                        ? Suits.SPADES
+                        : undefined,
+                  })
+                }
+              >
+                {t("game.deck.suit.spade").toUpperCase()}
+              </Button>
+              <Button
+                size={"sm"}
+                variant={
+                  filterButtonsState.suit === Suits.HEARTS
+                    ? "outlineSecondaryGlowActive"
+                    : "outlineSecondaryGlow"
+                }
+                borderRadius={"25px"}
+                onClick={() =>
+                  updateFilters({
+                    suit:
+                      filterButtonsState.suit !== Suits.HEARTS
+                        ? Suits.HEARTS
+                        : undefined,
+                  })
+                }
+              >
+                {t("game.deck.suit.heart").toUpperCase()}
+              </Button>
+              <Button
+                size={"sm"}
+                variant={
+                  filterButtonsState.suit === Suits.DIAMONDS
+                    ? "outlineSecondaryGlowActive"
+                    : "outlineSecondaryGlow"
+                }
+                borderRadius={"25px"}
+                onClick={() =>
+                  updateFilters({
+                    suit:
+                      filterButtonsState.suit !== Suits.DIAMONDS
+                        ? Suits.DIAMONDS
+                        : undefined,
+                  })
+                }
+              >
+                {t("game.deck.suit.diamond").toUpperCase()}
+              </Button>
+              <Button
+                size={"sm"}
+                variant={
+                  filterButtonsState.isNeon
+                    ? "outlineSecondaryGlowActive"
+                    : "outlineSecondaryGlow"
+                }
+                borderRadius={"25px"}
+                onClick={() =>
+                  updateFilters({
+                    isNeon: !filterButtonsState.isNeon ? true : undefined,
+                  })
+                }
+              >
+                {t("game.deck.suit.neon").toUpperCase()}
+              </Button>
+              <Button
+                size={"sm"}
+                variant={
+                  filterButtonsState.isModifier
+                    ? "outlineSecondaryGlowActive"
+                    : "outlineSecondaryGlow"
+                }
+                borderRadius={"25px"}
+                onClick={() =>
+                  updateFilters({
+                    isModifier: !filterButtonsState.isModifier
+                      ? true
+                      : undefined,
+                  })
+                }
+              >
+                {t("game.deck.suit.modifier").toUpperCase()}
+              </Button>
+            </Flex>
+          </Flex>
+        </Flex>
 
-                    <Flex gap={4} mt={4} wrap={"wrap"} justifyContent={"center"}>
-                            <Button
-                                variant={"outlinePrimaryGlow"}
-                                onClick={() => {
-                                    setSpecialCardsModalOpen(true);
-                                }}
-                            >
-                                SEE SPECIAL CARDS
-                            </Button>
-                            <Button 
-                                variant={"outlinePrimaryGlow"} 
-                                onClick={() => navigate("/demo")}
-                            >
-                                BACK TO GAME
-                            </Button>
-                        </Flex>
-                </Flex>
-            </>
-        );
-    }
+        <Flex
+          alignItems={"center"}
+          width={"95%"}
+          height={"60%"}
+          overflowY="auto"
+        >
+          <Box w="100%" h="100%">
+            <DeckCardsGrid
+              cards={fullDeck}
+              usedCards={usedCards}
+              filters={{
+                isNeon: filterButtonsState.isNeon,
+                isModifier: filterButtonsState.isModifier,
+                suit: filterButtonsState.suit ?? undefined,
+              }}
+            />
+          </Box>
+        </Flex>
+
+        <Flex gap={4} mt={4} wrap={"wrap"} justifyContent={"center"}>
+          <Button
+            variant={"outlinePrimaryGlow"}
+            onClick={() => {
+              setSpecialCardsModalOpen(true);
+            }}
+          >
+            {t("game.deck.btns.special-cards").toUpperCase()}
+          </Button>
+          <Button
+            variant={"outlinePrimaryGlow"}
+            onClick={() => navigate("/demo")}
+          >
+            {t("game.deck.btns.back").toUpperCase()}
+          </Button>
+        </Flex>
+      </Flex>
+    </>
+  );
+};
