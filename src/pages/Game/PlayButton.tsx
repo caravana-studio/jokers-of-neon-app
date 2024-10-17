79ef2fb3b27ad6f59256ae5a50acb4c5325ae141
brendamareco <brendaamareco@gmail.com>
2024-10-16 21:40:33 -0300

    changed isMobile for isSmallScreen on buttons

--- src/pages/Game/PlayButton.tsx
+++ src/pages/Game/PlayButton.tsx
@@ -1,28 +1,33 @@
 import { Box, Button, Heading, Text } from "@chakra-ui/react";
-import { isMobile } from "react-device-detect";
 import { useRound } from "../../dojo/queries/useRound";
 import { useGameContext } from "../../providers/GameProvider";
 import { ButtonContainer } from "./ButtonContainer";
 import { useTranslation } from "react-i18next";
+import { useResponsiveValues } from "../../theme/responsiveSettings";
 
 interface PlayButtonProps {
   highlight?: boolean;
 }
 
 export const PlayButton = ({ highlight = false }: PlayButtonProps) => {
-  const { preSelectedCards, play, preSelectionLocked } =
-    useGameContext();
+  const { preSelectedCards, play, preSelectionLocked } = useGameContext();
 
   const round = useRound();
   const handsLeft = round?.hands ?? 0;
 
-  const cantPlay = !highlight && (preSelectionLocked || preSelectedCards?.length === 0 || !handsLeft || handsLeft === 0 );
+  const cantPlay =
+    !highlight &&
+    (preSelectionLocked ||
+      preSelectedCards?.length === 0 ||
+      !handsLeft ||
+      handsLeft === 0);
   const { t } = useTranslation(["game"]);
+  const { isSmallScreen } = useResponsiveValues();
 
   return (
     <ButtonContainer>
       <Button
-        width={isMobile ? "48%" : "170px"}
+        width={isSmallScreen ? "48%" : "170px"}
         onClick={(e) => {
           e.stopPropagation();
           play();
@@ -31,20 +36,28 @@ export const PlayButton = ({ highlight = false }: PlayButtonProps) => {
         isDisabled={cantPlay}
         className="game-tutorial-step-4"
       >
-        {isMobile ? (
+        {isSmallScreen ? (
           <Box>
             <Text fontFamily="Orbitron" fontSize={16} height={"16px"}>
-              {t('game.preselected-cards-section.play-btn-lbl.play-mobile')}
+              {t("game.preselected-cards-section.play-btn-lbl.play-mobile")}
             </Text>
             <Heading mt={1} fontSize={9}>
-              {t('game.preselected-cards-section.play-btn-lbl.left', {handsLeft: handsLeft})}
+              {t("game.preselected-cards-section.play-btn-lbl.left", {
+                handsLeft: handsLeft,
+              })}
             </Heading>
           </Box>
         ) : (
-          t('game.preselected-cards-section.play-btn-lbl.play')
+          t("game.preselected-cards-section.play-btn-lbl.play")
         )}
       </Button>
-      {!isMobile && <Text size="l">{t('game.preselected-cards-section.play-btn-lbl.left', {handsLeft: handsLeft})}</Text>}
+      {!isSmallScreen && (
+        <Text size="l">
+          {t("game.preselected-cards-section.play-btn-lbl.left", {
+            handsLeft: handsLeft,
+          })}
+        </Text>
+      )}
     </ButtonContainer>
   );
 };
