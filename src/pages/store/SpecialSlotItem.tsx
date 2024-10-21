ec3ac009a25a2ed227897af490d75731733c1a42
nicon44 <nicolas@dulio.com.ar>
2024-10-21 12:02:35 +0200

    hide button if max

--- src/pages/store/SpecialSlotItem.tsx
+++ src/pages/store/SpecialSlotItem.tsx
@@ -7,6 +7,8 @@ import { PriceBox } from "../../components/PriceBox";
 import { CARD_HEIGHT, CARD_WIDTH } from "../../constants/visualProps";
 import { useSpecialCardSlotItem } from "../../dojo/queries/useSpecialCardSlotItem";
 import { useCardScale } from "../../hooks/useCardScale";
+import { useGame } from "../../dojo/queries/useGame";
+import { MAX_SPECIAL_CARDS } from "../../constants/config";
 
 interface ISpecialSlotItem {}
 
@@ -16,6 +18,10 @@ export const SpecialSlotItem = ({}: ISpecialSlotItem) => {
   const specialSlotItem = useSpecialCardSlotItem();
   const navigate = useNavigate();
 
+  const game = useGame();
+  const visible =
+    (game?.len_max_current_special_cards ?? 1) < MAX_SPECIAL_CARDS;
+
   const price = specialSlotItem?.cost;
   const purchased = specialSlotItem?.purchased ?? false;
 
@@ -23,41 +29,41 @@ export const SpecialSlotItem = ({}: ISpecialSlotItem) => {
   const width = CARD_WIDTH * scale * 0.8;
 
   return (
-    <Tooltip label={t("tooltip")} placement="top">
-      <Flex
-        position="relative"
-        height={`${height}px`}
-        width={`${width}px`}
-        cursor={purchased ? "not-allowed" : "pointer"}
-        onClick={() => {
-          if (!purchased) {
-            navigate("/preview/slot");
-          }
-        }}
-      >
-        <CachedImage
-          opacity={purchased ? 0.3 : 1}
-          src="/store/slot-icon.png"
-          alt="slot-icon"
-        />
-        {price && (
-          <PriceBox price={price} purchased={purchased} />
-        )}
-        {purchased && (
-          <Box
-            sx={{
-              position: "absolute",
-              top: `${height / 2 - 10}px`,
-              left: 0,
-              zIndex: 10,
-            }}
-          >
-            <Heading variant="italic" fontSize={isMobile ? 6 : 11 * scale}>
-              PURCHASED
-            </Heading>
-          </Box>
-        )}
-      </Flex>
-    </Tooltip>
+    visible && (
+      <Tooltip label={t("tooltip")} placement="top">
+        <Flex
+          position="relative"
+          height={`${height}px`}
+          width={`${width}px`}
+          cursor={purchased ? "not-allowed" : "pointer"}
+          onClick={() => {
+            if (!purchased) {
+              navigate("/preview/slot");
+            }
+          }}
+        >
+          <CachedImage
+            opacity={purchased ? 0.3 : 1}
+            src="/store/slot-icon.png"
+            alt="slot-icon"
+          />
+          {price && <PriceBox price={price} purchased={purchased} />}
+          {purchased && (
+            <Box
+              sx={{
+                position: "absolute",
+                top: `${height / 2 - 10}px`,
+                left: 0,
+                zIndex: 10,
+              }}
+            >
+              <Heading variant="italic" fontSize={isMobile ? 6 : 11 * scale}>
+                PURCHASED
+              </Heading>
+            </Box>
+          )}
+        </Flex>
+      </Tooltip>
+    )
   );
 };
