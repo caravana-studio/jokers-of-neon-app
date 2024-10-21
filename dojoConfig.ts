45ea9910a5f48dac4d8c25e37da46b84d75b6b29
nicon44 <nicolas@dulio.com.ar>
2024-10-21 23:54:33 +0200

    rollback dojoconfig

--- dojoConfig.ts
+++ dojoConfig.ts
@@ -1,5 +1,5 @@
 import { createDojoConfig } from "@dojoengine/core";
-import manifest from "./src/manifest.json";
+import manifest from "../jokers-of-neon-contracts/manifests/dev/deployment/manifest.json";
 
 const rpcUrl = import.meta.env.VITE_RPC_URL || "http://localhost:5050"
 const toriiUrl = import.meta.env.VITE_TORII_URL || "http://localhost:8080"
