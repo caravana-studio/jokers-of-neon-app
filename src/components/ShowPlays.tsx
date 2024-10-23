00992c76bc5d8e0f4c6db3b1d581f51954b8d727
Cristian Fleita <cristianagustinfleita@gmail.com>
2024-10-23 09:10:10 -0300

    chore: add state to show plays

--- src/components/ShowPlays.tsx
+++ src/components/ShowPlays.tsx
@@ -4,41 +4,41 @@ import { isMobile } from "react-device-detect";
 import { Button, Flex, Text } from "@chakra-ui/react";
 import { InfoIcon } from "@chakra-ui/icons";
 import { useTranslation } from "react-i18next";
+import { isTutorial } from "../utils/isTutorial";
 
 export const ShowPlays = () => {
-    const [hoveredButton, setHoveredButton] = useState(false);
-    const navigate = useNavigate();
-    const { t } = useTranslation(["game"]);
+  const [hoveredButton, setHoveredButton] = useState(false);
+  const navigate = useNavigate();
+  const { t } = useTranslation(["game"]);
+  const inTutorial = isTutorial();
 
-    return (
-        <Button
-            variant={hoveredButton ? "solid" : "defaultOutline"}
-            borderRadius={"8px"}
-            height={isMobile ? 6 : 8}
-            borderColor = "transparent !important"
-            _hover={{
-                borderColor: "white !important"
-            }}
-            size={"xs"}
-            className="game-tutorial-step-5"
-            onMouseEnter={() => setHoveredButton(true)}
-            onMouseLeave={() => setHoveredButton(false)}
-            onClick={(e) => {
-                e.stopPropagation();
-                navigate("/plays");
-            }}
-            >
-                <Flex gap={2} alignItems={"center"} justifyContent={"space-between"}>
-                <InfoIcon
-                    color="white"
-                    fontSize={{ base: "14px", md: "20px" }}
-                    sx={{ cursor: "pointer" }}
-                    className="game-tutorial-step-5"
-                />
-                <Text textTransform="initial">
-                    {t('game.hand-section.show-plays')}
-                </Text>
-                </Flex>
-        </Button>
-    );
-}
+  return (
+    <Button
+      variant={hoveredButton ? "solid" : "defaultOutline"}
+      borderRadius={"8px"}
+      height={isMobile ? 6 : 8}
+      borderColor="transparent !important"
+      _hover={{
+        borderColor: "white !important",
+      }}
+      size={"xs"}
+      className="game-tutorial-step-5"
+      onMouseEnter={() => setHoveredButton(true)}
+      onMouseLeave={() => setHoveredButton(false)}
+      onClick={(e) => {
+        e.stopPropagation();
+        navigate("/plays", { state: { isTutorial: inTutorial } });
+      }}
+    >
+      <Flex gap={2} alignItems={"center"} justifyContent={"space-between"}>
+        <InfoIcon
+          color="white"
+          fontSize={{ base: "14px", md: "20px" }}
+          sx={{ cursor: "pointer" }}
+          className="game-tutorial-step-5"
+        />
+        <Text textTransform="initial">{t("game.hand-section.show-plays")}</Text>
+      </Flex>
+    </Button>
+  );
+};
