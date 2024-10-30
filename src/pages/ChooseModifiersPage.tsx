b97b6e5671670f1aff65bb3961c8871005008beb
brendamareco <brendaamareco@gmail.com>
2024-10-30 17:57:34 -0300

    set selectedcards to empty array

--- src/pages/ChooseModifiersPage.tsx
+++ src/pages/ChooseModifiersPage.tsx
@@ -35,7 +35,6 @@ export const ChooseModifiersPage = () => {
       setCards([]);
     } else {
       setCards(blisterPackResult?.cards ?? []);
-      setCardsToKeep(blisterPackResult?.cards ?? []);
     }
   }, [blisterPackResult]);
 
--- src/pages/ChooseSpecialsPage.tsx
+++ src/pages/ChooseSpecialsPage.tsx
@@ -34,7 +34,6 @@ export const ChooseSpecialsPage = () => {
       setCards([]);
     } else {
       setCards(blisterPackResult?.cards ?? []);
-      setCardsToKeep(blisterPackResult?.cards ?? []);
     }
   }, [blisterPackResult]);
 
