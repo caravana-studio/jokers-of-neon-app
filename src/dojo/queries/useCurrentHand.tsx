326368228add14e424b2c4e22660bf3a00198b4f
dub_zn <santiagodgalvan@gmail.com>
2024-11-12 20:08:17 -0300

    refactor current hand

--- src/dojo/queries/useCurrentHand.tsx
+++ src/dojo/queries/useCurrentHand.tsx
@@ -10,6 +10,13 @@ import { sortCards } from "../../utils/sortCards";
 import { useDojo } from "../useDojo";
 import { useGame } from "./useGame";
 
+const getCurrentHandCards = (gameId: number, entity: Component) => {
+  const entityId = getEntityIdFromKeys([
+    BigInt(gameId),
+  ]) as Entity;
+  return getComponentValue(entity, entityId);
+}
+
 const getCard = (gameId: number, index: number, entity: Component) => {
   const entityId = getEntityIdFromKeys([
     BigInt(gameId),
@@ -21,7 +28,7 @@ const getCard = (gameId: number, index: number, entity: Component) => {
 export const useCurrentHand = (sortBy: SortBy) => {
   const {
     setup: {
-      clientComponents: { CurrentHandCard },
+      clientComponents: { CurrentHand },
     },
   } = useDojo();
 
@@ -29,26 +36,27 @@ export const useCurrentHand = (sortBy: SortBy) => {
 
   if (!game) return [];
 
-  const handSize = game.len_hand;
-  const dojoCards = [];
+  const dojoCards = getCurrentHandCards(game.id ?? 0, CurrentHand)?.cards ?? [];
 
-  for (let i = 0; i < handSize; i++) {
-    dojoCards.push(getCard(game.id ?? 0, i, CurrentHandCard));
-  }
-
+  console.log("dojoCards: ", dojoCards);
   const cards: Card[] = dojoCards
-    // filter out null cards (represented by card_id 9999)
-    .filter((card) => card?.card_id !== 9999)
-    .map((dojoCard) => {
+    // Filter out null cards (where card_id is represented by the value 9999)
+    .filter((card: any) => card.value !== 9999)
+    .map((dojoCard: any, idx: number) => {
+      const cardId = dojoCard.value; // Extract card_id from value
       return {
         ...dojoCard,
-        img: `${dojoCard?.card_id}.png`,
-        isModifier: dojoCard?.card_id >= 600 && dojoCard?.card_id <= 700,
-        idx: dojoCard?.idx,
-        id: dojoCard?.idx.toString(),
+        card_id: cardId,
+        img: `${cardId}.png`,
+        isModifier: cardId >= 600 && cardId <= 700,
+        idx: idx, // Using index if `idx` is not already defined in dojoCard
+        id: idx.toString(),
       };
     });
-
+  
+  console.log("Filtered and mapped cards: ", cards);
   const sortedCards = sortCards(cards, sortBy);
+  console.log("sortedCards: ", sortedCards);
+
   return sortedCards;
 };
