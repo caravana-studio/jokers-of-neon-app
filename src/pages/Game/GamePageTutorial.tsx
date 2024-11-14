79a3c42c28f9057084818f80bb12767da78fbdf5
Cristian Fleita <cristianagustinfleita@gmail.com>
2024-11-14 13:52:21 -0300

    fix: useEffect

--- src/pages/Game/GamePageTutorial.tsx
+++ src/pages/Game/GamePageTutorial.tsx
@@ -5,9 +5,13 @@ import { GameContent } from "./GameContent";
 import { MobileGameContent } from "./GameContent.mobile";
 import { PositionedDiscordLink } from "../../components/DiscordLink";
 import { SKIP_IN_GAME_TUTORIAL } from "../../constants/localStorage";
+import { useEffect } from "react";
 
 export const GamePageTutorial = () => {
-  window.localStorage.setItem(SKIP_IN_GAME_TUTORIAL, "true");
+  useEffect(() => {
+    window.localStorage.setItem(SKIP_IN_GAME_TUTORIAL, "true");
+  });
+
   return (
     <Background type={"game"}>
       {isMobile ? <MobileGameContent /> : <GameContent />}
