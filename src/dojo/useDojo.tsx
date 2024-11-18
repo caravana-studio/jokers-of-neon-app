6f4f17a2cfb259bb39155f976b6134cbe6b744d4
Cristian Fleita <cristianagustinfleita@gmail.com>
2024-11-18 14:16:34 -0300

    chore: enable queries

--- src/dojo/useDojo.tsx
+++ src/dojo/useDojo.tsx
@@ -2,15 +2,13 @@ import { useContext } from "react";
 import { DojoContext } from "./DojoContext";
 
 export const useDojo = () => {
-    const context = useContext(DojoContext);
-    if (!context)
-        throw new Error(
-            "The `useDojo` hook must be used within a `DojoProvider`"
-        );
+  const context = useContext(DojoContext);
+  if (!context)
+    throw new Error("The `useDojo` hook must be used within a `DojoProvider`");
 
-    return {
-        setup: context,
-        account: context.account,
-        // syncCall: context.syncCallback
-    };
+  return {
+    setup: context,
+    account: context.account,
+    syncCall: context.syncCallback,
+  };
 };
--- src/providers/GameProvider.tsx
+++ src/providers/GameProvider.tsx
@@ -11,7 +11,7 @@ import {
   SETTINGS_ANIMATION_SPEED,
   SETTINGS_SFX_VOLUME,
   SFX_ON,
-  SORT_BY_SUIT
+  SORT_BY_SUIT,
 } from "../constants/localStorage";
 import { rageCardIds } from "../constants/rageCardIds.ts";
 import {
@@ -167,7 +167,7 @@ export const GameProvider = ({ children }: PropsWithChildren) => {
       clientComponents: { Game },
     },
     account: { account },
-    // syncCall,
+    syncCall,
   } = useDojo();
 
   const { createGame, play, discard, discardEffectCard, discardSpecialCard } =
@@ -260,7 +260,7 @@ export const GameProvider = ({ children }: PropsWithChildren) => {
           clearPreSelection();
           localStorage.setItem(GAME_ID, newGameId.toString());
           console.log(`game ${newGameId} created`);
-          // await syncCall();
+          await syncCall();
           setGameLoading(false);
           setPreSelectionLocked(false);
           setRoundRewards(undefined);
