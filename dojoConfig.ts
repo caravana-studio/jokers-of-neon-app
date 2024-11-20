37145648c3760a352a008ae0fc9fbf1fff530c00
nicon44 <nicolas@dulio.com.ar>
2024-11-21 00:02:51 +0100

    fix burners

--- dojoConfig.ts
+++ dojoConfig.ts
@@ -10,12 +10,6 @@ const masterAddress =
 const masterPrivateKey =
   import.meta.env.VITE_MASTER_PRIVATE_KEY ||
   "0x1800000000300000180000000000030000000000003006001800006600";
-const accountClassHash =
-  import.meta.env.VITE_PUBLIC_ACCOUNT_CLASS_HASH ||
-  "0x079d9ce84b97bcc2a631996c3100d57966fc2f5b061fb1ec4dfd0040976bcac6";
-const feeTokenAddress =
-  import.meta.env.VITE_NETWORK_FEE_TOKEN ||
-  "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7";
 
 export const dojoConfig = createDojoConfig({
   manifest,
@@ -24,6 +18,4 @@ export const dojoConfig = createDojoConfig({
   relayUrl,
   masterAddress,
   masterPrivateKey,
-  accountClassHash,
-  feeTokenAddress,
 });
--- src/dojo/setup.ts
+++ src/dojo/setup.ts
@@ -1,15 +1,15 @@
 import { DojoConfig, DojoProvider } from "@dojoengine/core";
 import { BurnerManager } from "@dojoengine/create-burner";
+import { Component, Metadata, Schema } from "@dojoengine/recs";
 import { setEntities, syncEntities } from "@dojoengine/state";
 import * as torii from "@dojoengine/torii-client";
 import { Account, ArraySignatureType } from "starknet";
+import { GAME_ID } from "../constants/localStorage";
 import { createClientComponents } from "./createClientComponents";
 import { createSystemCalls } from "./createSystemCalls";
 import { setupWorld } from "./typescript/contracts.gen";
 import { defineContractComponents } from "./typescript/models.gen";
 import { world } from "./world";
-import { Component, Metadata, Schema } from "@dojoengine/recs";
-import { GAME_ID } from "../constants/localStorage";
 
 export type SetupResult = Awaited<ReturnType<typeof setup>>;
 
