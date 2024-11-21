15b205bf4d753717781a54b5101f3b7113b9e791
brendamareco <brendaamareco@gmail.com>
2024-11-21 02:26:22 -0300

    added more aspect ratios base scales

--- src/theme/responsiveSettings.tsx
+++ src/theme/responsiveSettings.tsx
@@ -10,24 +10,57 @@ export const useResponsiveValues = () => {
   const [specialCardScale, setSpecialCardScale] = useState<number>(1);
   const [isCardScaleCalculated, setIsCardScaleCalculated] = useState(false);
 
+  const referenceWidthDesktop = 1920;
+  const referenceHeightDesktop = 1080;
+
   const baseScales: { [key: string]: number } = {
-    "16:10": 2,
+    "375:667": 3.8,
+    "768:1423": 2.5,
+    "16:10": 1.4,
+    "2:1": 1.4,
+    "1.85:1": 1.4,
+    "1.96:1": 1.4,
+    "17:9": 1.8,
+    "126:65": 1.8,
+    "130:69": 1.4,
+    "151:80": 1.4,
+    "1774:937": 1.4,
+    "1938:937": 1.3,
     "4:3": 1.6,
     "21:9": 1,
-    "3:4": 2.5,
-    "9:16": 3.6,
-    "8:7": 2,
+    "3:2": 1.4,
+    "5:3": 1.5,
+    "3:4": 2,
+    "9:16": 2.5,
+    "8:7": 1.9,
     "1024:1127": 2,
+    "2.165:1": 1.35,
+    "19:9": 1.2,
+    "22:9": 1.2,
+    "32:10": 1,
   };
 
   const specialBaseScales: { [key: string]: number } = {
-    "16:10": 1.1,
-    "4:3": 1.0,
-    "21:9": 1.5,
-    "3:4": 1.2,
+    "375:667": 3.7,
+    "768:1423": 2.5,
+    "16:10": 1.2,
+    "1.58:1": 0.9,
+    "1.61:1": 0.9,
+    "1.96:1": 0.9,
+    "2:1": 0.9,
+    "17:9": 1,
+    "126:65": 1.8,
+    "4:3": 0.85,
+    "21:9": 0.7,
+    "3:2": 0.9,
+    "5:3": 0.8,
+    "3:4": 1.7,
     "9:16": 2,
-    "8:7": 1.2,
-    "1024:1127": 1.2,
+    "8:7": 0.8,
+    "1024:1127": 1.1,
+    "2.165:1": 0.7,
+    "22:9": 1,
+    "32:10": 1,
   };
 
   const getBaseScaleForAspectRatio = (
@@ -36,11 +69,13 @@ export const useResponsiveValues = () => {
   ): number => {
     const scales = isSpecialCard ? specialBaseScales : baseScales;
 
+    const TOLERANCE = 0.0001;
+
     for (const key in scales) {
       const [width, height] = key.split(":").map(Number);
       const aspectRatio = width / height;
 
-      if (Math.abs(aspectRatio - ratio) < 0.0001) {
+      if (Math.abs(aspectRatio - ratio) < TOLERANCE) {
         return scales[key];
       }
     }
@@ -61,6 +96,7 @@ export const useResponsiveValues = () => {
       }
     });
 
+    // If no closest match, return 1 (or a fallback scale of your choice)
     return scales[closestKey] || 1;
   };
 
@@ -68,11 +104,6 @@ export const useResponsiveValues = () => {
     const handleResize = throttle(() => {
       const newAspectRatio = window.innerWidth / window.innerHeight;
       setAspectRatio(newAspectRatio);
-
-      console.log("aspect ratio: " + newAspectRatio);
-      console.log(
-        "base scale ratio: " + getBaseScaleForAspectRatio(newAspectRatio, false)
-      );
 
       const baseScale = getBaseScaleForAspectRatio(newAspectRatio, false);
 
