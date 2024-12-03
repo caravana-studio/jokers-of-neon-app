c7254ad9d8ea246bef6eb2575361763292236312
brendamareco <brendaamareco@gmail.com>
2024-12-03 02:54:29 -0300

    Added drag and drop card change when using modifiers

--- src/utils/changeCardNeon.ts
+++ src/utils/changeCardNeon.ts
@@ -1,4 +1,4 @@
 
-export const getNeonCardId = (traditional_card_id: number): number => {
+export const changeCardNeon = (traditional_card_id: number): number => {
   return traditional_card_id + 200;
 };
