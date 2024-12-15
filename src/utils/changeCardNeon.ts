5b70b560b05c3107bcb96f77ed2d840f1001ad03
nicon44 <nicolas@dulio.com.ar>
2024-12-15 12:12:45 -0300

    rollback card id change

--- src/utils/changeCardNeon.ts
+++ src/utils/changeCardNeon.ts
@@ -1,6 +1,6 @@
 
 export const changeCardNeon = (traditional_card_id: number): number => {
-  if (traditional_card_id >= 0 && traditional_card_id < 100)
+  if (traditional_card_id >= 0 && traditional_card_id < 53)
     return traditional_card_id + 200;
   
   return traditional_card_id;
