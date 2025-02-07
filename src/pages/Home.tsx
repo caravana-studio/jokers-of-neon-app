93fc17f63f26a9b75d8cfc1c3ddb5d19388513d5
nicon44 <nicolas@dulio.com.ar>
2025-02-07 15:43:41 +0100

    remove unused imports

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,10 +6,8 @@ import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import AudioPlayer from "../components/AudioPlayer";
 import { Background } from "../components/Background";
-import CountdownTimer from "../components/CountdownTimer";
 import { DiscordLink } from "../components/DiscordLink";
 import LanguageSwitcher from "../components/LanguageSwitcher";
-import { Leaderboard } from "../components/Leaderboard";
 import { PoweredBy } from "../components/PoweredBy";
 import { useDojo } from "../dojo/useDojo";
 
@@ -54,60 +52,60 @@ export const Home = () => {
         alignItems="center"
         gap={4}
       >
-          <Flex
-            flexDirection="column"
-            alignItems="center"
-            gap={{ base: 6, sm: 8, md: 6 }}
-            w={"100%"}
-            zIndex={1}
+        <Flex
+          flexDirection="column"
+          alignItems="center"
+          gap={{ base: 6, sm: 8, md: 6 }}
+          w={"100%"}
+          zIndex={1}
+        >
+          <Heading
+            size="xl"
+            color="white"
+            fontSize={{ base: 10, sm: 20, md: 25, lg: 30 }}
           >
-            <Heading
-              size="xl"
-              color="white"
-              fontSize={{ base: 10, sm: 20, md: 25, lg: 30 }}
-            >
-              {t("home.slogan")}
-            </Heading>
+            {t("home.slogan")}
+          </Heading>
 
-            <Flex
-              w={"100%"}
-              justifyContent="center"
-              minH={isSmallScreen ? "unset" : "40vh"}
-              flexGrow={1}
-              maxWidth={isMobile ? "70%" : "50%"}
-            >
-              <Flex h={"100%"} w="100%" justifyContent={"center"} pl={2}>
-                <SpineAnimation
-                  jsonUrl={`/spine-animations/logo/JokerLogo.json`}
-                  atlasUrl={`/spine-animations/logo/JokerLogo.atlas`}
-                  initialAnimation={"animation"}
-                  loopAnimation={"animation"}
-                  scale={2.8}
-                  yOffset={-800}
-                />
-              </Flex>
-            </Flex>
-
-            <Flex
-              gap={{ base: 4, sm: 6 }}
-              flexWrap={{ base: "wrap", sm: "nowrap" }}
-            >
-              <Button
-                variant="secondarySolid"
-                onClick={() => {
-                  if (isDev) {
-                    navigate(enableMods ? "/mods" : "/login");
-                  } else {
-                    setPlayButtonClicked(true);
-                    connect({ connector: connectors[0] });
-                  }
-                }}
-                minW={["150px", "300px"]}
-              >
-                {t("home.btn.start")}
-              </Button>
+          <Flex
+            w={"100%"}
+            justifyContent="center"
+            minH={isSmallScreen ? "unset" : "40vh"}
+            flexGrow={1}
+            maxWidth={isMobile ? "70%" : "50%"}
+          >
+            <Flex h={"100%"} w="100%" justifyContent={"center"} pl={2}>
+              <SpineAnimation
+                jsonUrl={`/spine-animations/logo/JokerLogo.json`}
+                atlasUrl={`/spine-animations/logo/JokerLogo.atlas`}
+                initialAnimation={"animation"}
+                loopAnimation={"animation"}
+                scale={2.8}
+                yOffset={-800}
+              />
             </Flex>
           </Flex>
+
+          <Flex
+            gap={{ base: 4, sm: 6 }}
+            flexWrap={{ base: "wrap", sm: "nowrap" }}
+          >
+            <Button
+              variant="secondarySolid"
+              onClick={() => {
+                if (isDev) {
+                  navigate(enableMods ? "/mods" : "/login");
+                } else {
+                  setPlayButtonClicked(true);
+                  connect({ connector: connectors[0] });
+                }
+              }}
+              minW={["150px", "300px"]}
+            >
+              {t("home.btn.start")}
+            </Button>
+          </Flex>
+        </Flex>
         <PoweredBy />
       </Flex>
       <Box
