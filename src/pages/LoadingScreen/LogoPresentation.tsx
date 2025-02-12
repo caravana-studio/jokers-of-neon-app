8f74fc66bf677d33e0c69825d327d2aef94256e9
brendamareco <brendaamareco@gmail.com>
2025-02-12 17:52:19 -0300

    Added caravana logo

--- src/pages/LoadingScreen/LogoPresentation.tsx
+++ src/pages/LoadingScreen/LogoPresentation.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, Image } from "@chakra-ui/react";
 import { FadeInOut } from "../../components/animations/FadeInOut";
 
 interface LogoPresentationProps {
@@ -12,16 +12,33 @@ export const LogoPresentation: React.FC<LogoPresentationProps> = ({
   visibleElements = { text: false, logo: false },
 }) => {
   return (
-    <Flex flexDirection="column" alignItems="center" gap={4}>
-      <FadeInOut isVisible={visibleElements.logo} fadeOut>
-        <img
+    <Flex
+      width={"100%"}
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      textAlign={"center"}
+      gap={4}
+    >
+      <FadeInOut
+        isVisible={visibleElements.logo}
+        fadeOut
+        fadeOutDelay={1}
+        fadeOutDuration={0.5}
+      >
+        <Image
           style={{ marginTop: "80px" }}
-          width="60%"
-          src="logos/logo.png"
+          width={["100%", "100%", "60%"]}
+          src="/logos/caravana-logo.png"
           alt="logo"
         />
       </FadeInOut>
-      <FadeInOut isVisible={visibleElements.text} fadeOut fadeInDelay={0.5}>
+      <FadeInOut
+        isVisible={visibleElements.text}
+        fadeOut
+        fadeOutDelay={1}
+        fadeOutDuration={0.5}
+      >
         <Text fontSize="lg">PRESENTS</Text>
       </FadeInOut>
     </Flex>
