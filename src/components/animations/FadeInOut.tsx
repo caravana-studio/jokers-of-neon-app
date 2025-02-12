9a733d65bc9663fe4ec9d83ffc70714b1ebaa6c3
brendamareco <brendaamareco@gmail.com>
2025-02-12 11:53:02 -0300

    Added fade in out component

---
+++ src/components/animations/FadeInOut.tsx
@@ -0,0 +1,38 @@
+import { motion } from "framer-motion";
+import { Box } from "@chakra-ui/react";
+
+interface FadeInOutProps {
+  isVisible: boolean;
+  fadeOut?: boolean;
+  fadeInDuration?: number;
+  fadeInDelay?: number;
+  fadeOutDelay?: number;
+  fadeOutDuration?: number;
+  children: React.ReactNode;
+}
+
+export const FadeInOut: React.FC<FadeInOutProps> = ({
+  isVisible,
+  fadeOut,
+  fadeInDuration = 0.5,
+  fadeInDelay = 0,
+  fadeOutDelay = 1,
+  fadeOutDuration = 0.5,
+  children,
+}) => (
+  <motion.div
+    initial={{ opacity: 0 }}
+    animate={{ opacity: isVisible ? 1 : 0 }}
+    exit={
+      fadeOut
+        ? {
+            opacity: 0,
+            transition: { duration: fadeOutDuration, delay: fadeOutDelay },
+          }
+        : undefined
+    }
+    transition={{ duration: fadeInDuration, delay: fadeInDelay }}
+  >
+    <Box>{children}</Box>
+  </motion.div>
+);
