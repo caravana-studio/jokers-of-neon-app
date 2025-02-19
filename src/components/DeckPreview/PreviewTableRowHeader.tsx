7f07f2087c0cbdb8daba367dcb90b83eb696f5bb
brendamareco <brendaamareco@gmail.com>
2025-02-18 23:37:06 -0300

    styled row header

--- src/components/DeckPreview/PreviewTableRowHeader.tsx
+++ src/components/DeckPreview/PreviewTableRowHeader.tsx
@@ -1,5 +1,6 @@
-import { Flex, Box } from "@chakra-ui/react";
+import { Flex, Box, useTheme } from "@chakra-ui/react";
 import { cardSuitsMap, RowHeader } from "./DeckPreviewTableUtils";
+import { GREY_LINE, GREY_MEDIUM } from "../../theme/colors";
 
 export const PreviewTableRowHeader: React.FC<RowHeader> = ({
   cardSuit,
@@ -10,16 +11,31 @@ export const PreviewTableRowHeader: React.FC<RowHeader> = ({
   const Icon = cardSuitsMap.get(cardSuit);
   if (!Icon) return null;
 
+  const { colors } = useTheme();
+
   return (
     <Flex
       alignItems={"center"}
       justifyContent={"center"}
-      gap={2}
-      mx={2}
+      px={4}
+      py={1}
+      columnGap={2}
       textAlign={"center"}
+      backgroundColor={"black"}
+      border={"1px"}
+      borderRadius={"15px"}
+      borderColor={"white"}
     >
-      {Icon && <Icon width={12} fill={"white"} height={"100%"} />}
-      <Box p={2} textAlign={"center"} borderRadius={"15px"}>
+      {Icon && <Icon width={12} fill={colors[cardSuit]} height={"100%"} />}
+      <Box
+        px={2}
+        textAlign={"center"}
+        borderRadius={"15px"}
+        border={"1px"}
+        borderColor={GREY_LINE}
+        color={"white"}
+        backgroundColor={GREY_MEDIUM}
+      >
         {quantity}
       </Box>
     </Flex>
