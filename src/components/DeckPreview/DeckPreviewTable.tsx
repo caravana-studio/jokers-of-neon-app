6755d5fce9b7c6ae6e61e3392b8a6a57b820eb3a
brendamareco <brendaamareco@gmail.com>
2025-02-19 01:03:41 -0300

    positioned table on deck hover

--- src/components/DeckPreview/DeckPreviewTable.tsx
+++ src/components/DeckPreview/DeckPreviewTable.tsx
@@ -21,68 +21,61 @@ export const DeckPreviewTable = () => {
   const { colors } = useTheme();
 
   return (
-    <Flex flexDirection={"column"} position={"absolute"} zIndex={100}>
-      <Table
-        variant="simple"
-        size="sm"
-        border="none"
-        borderWidth={0}
-        cellSpacing={"0 8px"}
-      >
-        <Thead>
-          <Tr>
-            <Th border="none"></Th>
-            {columnHeaders.map((header, index) => (
-              <Th key={index} border="none" padding={0} paddingLeft={1}>
-                <PreviewTableColumnHeader
-                  cardValue={header.cardValue}
-                  quantity={header.quantity}
-                />
-              </Th>
-            ))}
-          </Tr>
-        </Thead>
-        <Tbody color={"white"}>
-          {rows.map((row, rowIndex) => (
-            <Tr
-              key={rowIndex}
-              border="none"
-              borderBottom="2px solid transparent"
-            >
-              <Th border="none" p={0} m={0} pr={1}>
-                <PreviewTableRowHeader
-                  cardSuit={rowHeaders[rowIndex].cardSuit}
-                  quantity={rowHeaders[rowIndex].quantity}
-                />
-              </Th>
-              {row.map((cell, cellIndex) => {
-                const suit = rowHeaders[rowIndex]?.cardSuit;
-                return (
-                  <Td
-                    key={`${rowIndex}-${cellIndex}`}
-                    p={1}
-                    textAlign="center"
-                    backgroundColor={suit ? colors[suit] : "transparent"}
-                    opacity={0.8}
-                    border="none"
-                    borderRadius={
-                      cellIndex === 0
-                        ? "15px 0 0 15px"
-                        : cellIndex === row.length - 1
-                          ? "0 15px 15px 0"
-                          : "0"
-                    }
-                  >
-                    <Text opacity={cell.quantity > 0 ? 1 : 0.6}>
-                      {cell.quantity}
-                    </Text>
-                  </Td>
-                );
-              })}
-            </Tr>
+    <Table
+      variant="simple"
+      size="sm"
+      border="none"
+      borderWidth={0}
+      cellSpacing={"0 8px"}
+    >
+      <Thead>
+        <Tr>
+          <Th border="none"></Th>
+          {columnHeaders.map((header, index) => (
+            <Th key={index} border="none" padding={0} paddingLeft={1}>
+              <PreviewTableColumnHeader
+                cardValue={header.cardValue}
+                quantity={header.quantity}
+              />
+            </Th>
           ))}
-        </Tbody>
-      </Table>
-    </Flex>
+        </Tr>
+      </Thead>
+      <Tbody color={"white"}>
+        {rows.map((row, rowIndex) => (
+          <Tr key={rowIndex} border="none" borderBottom="2px solid transparent">
+            <Th border="none" p={0} m={0} pr={1}>
+              <PreviewTableRowHeader
+                cardSuit={rowHeaders[rowIndex].cardSuit}
+                quantity={rowHeaders[rowIndex].quantity}
+              />
+            </Th>
+            {row.map((cell, cellIndex) => {
+              const suit = rowHeaders[rowIndex]?.cardSuit;
+              return (
+                <Td
+                  key={`${rowIndex}-${cellIndex}`}
+                  p={1}
+                  textAlign="center"
+                  backgroundColor={suit ? colors[suit] : "transparent"}
+                  border="none"
+                  borderRadius={
+                    cellIndex === 0
+                      ? "15px 0 0 15px"
+                      : cellIndex === row.length - 1
+                        ? "0 15px 15px 0"
+                        : "0"
+                  }
+                >
+                  <Text opacity={cell.quantity > 0 ? 1 : 0.6}>
+                    {cell.quantity}
+                  </Text>
+                </Td>
+              );
+            })}
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
   );
 };
