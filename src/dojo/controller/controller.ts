0a362096b80fc48df95a2b3929cb0a9b3994a79b
nicon44 <nicolas@dulio.com.ar>
2025-03-07 15:52:07 +0100

    fix local not controller

--- src/dojo/controller/controller.ts
+++ src/dojo/controller/controller.ts
@@ -11,13 +11,17 @@ const defaultChainId = shortString.encodeShortString(
   `WP_${SLOT.toUpperCase().replace("-", "_")}`
 );
 
+const isDev = import.meta.env.VITE_DEV === "true";
+
 const RPC_URL = import.meta.env.VITE_RPC_URL || "http://localhost:5050";
 
-export const controller = new ControllerConnector({
-  policies,
-  chains: [{ rpcUrl: RPC_URL }],
-  defaultChainId,
-  namespace: DOJO_NAMESPACE,
-  slot: SLOT,
-  preset: "jokers-of-neon",
-});
+export const controller =
+  !isDev &&
+  new ControllerConnector({
+    policies,
+    chains: [{ rpcUrl: RPC_URL }],
+    defaultChainId,
+    namespace: DOJO_NAMESPACE,
+    slot: SLOT,
+    preset: "jokers-of-neon",
+  });
