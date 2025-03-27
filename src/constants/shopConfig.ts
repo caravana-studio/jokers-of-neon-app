7b277275a50503908d85d82625318d8cde91e72c
brendamareco <brendaamareco@gmail.com>
2025-03-27 17:45:10 -0300

    Added page title translations

--- src/constants/shopConfig.ts
+++
@@ -1,9 +0,0 @@
-export const SHOP_CONFIG: Record<number, string> = {
-    [0]: "",
-    [1]: "Deck",
-    [2]: "Global",
-    [3]: "Specials",
-    [4]: "Level ups",
-    [5]: "Modifiers",
-    [6]: "Mix",
-}
--- src/hooks/useCurrentPageName.tsx
+++ src/hooks/useCurrentPageName.tsx
@@ -1,9 +1,9 @@
 import { useLocation } from "react-router-dom";
 import { useGameContext } from "../providers/GameProvider";
 import { useGame } from "../dojo/queries/useGame";
-import { SHOP_CONFIG } from "../constants/shopConfig";
 import { useEffect, useState } from "react";
 import { LAST_PAGE } from "../constants/localStorage";
+import { useTranslation } from "react-i18next";
 
 interface Page {
   name: string;
@@ -16,6 +16,8 @@ export const useCurrentPageName = (): Page | null => {
   const { isRageRound } = useGameContext();
   const level = game?.level ?? 0;
   const shopConfigId = game?.shop_config_id ?? 0;
+  const { t: tGame } = useTranslation(["game"], { keyPrefix: "game" });
+  const { t: tShop } = useTranslation(["store"]);
 
   const [currentPage, setCurrentPage] = useState<Page | null>(() => {
     const storedPage = localStorage.getItem(LAST_PAGE);
@@ -23,10 +25,21 @@ export const useCurrentPageName = (): Page | null => {
   });
 
   const pageMap: Record<string, string | (() => string)> = {
-    "/gameover": "Game Over",
-    "/open-loot-box": "Open loot box",
-    "/store": "Rewards - " + SHOP_CONFIG[shopConfigId],
-    "/demo": () => `${isRageRound ? "Rage Round" : "Round"} - ${level}`,
+    "/gameover": tGame("game-menu.pages.gameover"),
+    "/open-loot-box": tGame("game-menu.pages.open-loot-box"),
+    "/store": () => {
+      const shopName = tShop(`config.${shopConfigId}.name`, {
+        defaultValue: "",
+      });
+      return tGame("game-menu.pages.store", { shopName });
+    },
+    "/demo": () =>
+      tGame("game-menu.pages.demo", {
+        roundType: isRageRound
+          ? tGame("game-menu.pages.rage-round")
+          : tGame("game-menu.pages.round"),
+        level,
+      }),
   };
 
   const pageNameResolver = pageMap[location.pathname];
