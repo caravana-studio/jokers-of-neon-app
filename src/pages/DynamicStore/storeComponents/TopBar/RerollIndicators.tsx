34ec2fadec8011d57edb083a515a5bc665b56a6a
brendamareco <brendaamareco@gmail.com>
2025-03-31 10:57:46 -0300

    Added reroll indicators

---
+++ src/pages/DynamicStore/storeComponents/TopBar/RerollIndicators.tsx
@@ -0,0 +1,34 @@
+import { Flex, Text } from "@chakra-ui/react";
+import { PlayDiscardIndicators } from "../../../Game/PlayDiscardIndicator";
+
+interface PlayDiscardIndicatorsProps {
+  rerolls: number;
+  rerollsActive: number;
+}
+
+export const RerollIndicators: React.FC<PlayDiscardIndicatorsProps> = ({
+  rerolls,
+  rerollsActive,
+}) => {
+  if (rerolls >= 5)
+    return (
+      <Flex gap={4} justifyContent={"center"} alignItems={"center"}>
+        <PlayDiscardIndicators
+          disabled={false}
+          type={"play"}
+          total={1}
+          active={rerollsActive === 0 ? 0 : 1}
+        />
+        <Text>X {rerollsActive}</Text>
+      </Flex>
+    );
+  else
+    return (
+      <PlayDiscardIndicators
+        disabled={false}
+        type={"play"}
+        total={rerolls}
+        active={rerollsActive}
+      />
+    );
+};
--- src/pages/DynamicStore/storeComponents/TopBar/RerollSection.tsx
+++ src/pages/DynamicStore/storeComponents/TopBar/RerollSection.tsx
@@ -3,19 +3,21 @@ import RerollButton from "../../../store/StoreElements/RerollButton";
 import { PlayDiscardIndicators } from "../../../Game/PlayDiscardIndicator";
 import { DefaultInfo } from "../../../../components/Info/DefaultInfo";
 import { MobileCoins } from "../../../store/Coins";
+import { RerollIndicators } from "./RerollIndicators";
 
 export const RerollSection = () => {
+  const rerollCant = 5;
+  const rerollActiveCant = 5;
   return (
     <Flex justifyContent="space-between" alignItems={"center"}>
-      <Flex gap={4} py={1}>
+      <Flex gap={4} py={1} alignItems={"center"}>
         <RerollButton />
-
-        <PlayDiscardIndicators
-          disabled={false}
-          type={"play"}
-          total={3}
-          active={3}
-        />
+        <Flex width={"100px"} ml={4}>
+          <RerollIndicators
+            rerolls={rerollCant}
+            rerollsActive={rerollActiveCant}
+          />
+        </Flex>
         <DefaultInfo title="reroll" />
       </Flex>
       <Flex>
