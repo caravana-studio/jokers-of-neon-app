069cf9e0ae5ec6b50039b9a821fbc9196c340864
nicon44 <nicolas@dulio.com.ar>
2025-04-10 16:01:28 +0200

    logs

--- src/dojo/useGameActions.tsx
+++ src/dojo/useGameActions.tsx
@@ -22,7 +22,8 @@ const CREATE_GAME_EVENT_KEY = getEventKey(DojoEvents.CREATE_GAME);
 
 //TODO: dehardcode this
 const MINT_GAME_EVENT_KEY =
-  "0x2f01dd863550300355e99ebfc08524ac0d60d424c59eda114a54140df28d8ac";
+  "0x77119cb21ea58e1c725435d6bc34e61905be768aa2ba7662ddebd46379b83dc";
+  // "0x2f01dd863550300355e99ebfc08524ac0d60d424c59eda114a54140df28d8ac";
 
 export const useGameActions = () => {
   const {
@@ -31,6 +32,8 @@ export const useGameActions = () => {
   } = useDojo();
 
   const createGame = async (gameId: number, username: string) => {
+    console.log("gameID", gameId);
+    console.log("username", username);
     try {
       showTransactionToast();
       const response = await client.game_system.createGame(
@@ -224,11 +227,8 @@ export const useGameActions = () => {
       updateTransactionToast(transaction_hash, tx.isSuccess());
       if (tx.isSuccess()) {
         const events = tx.events;
-        const gameId = getNumberValueFromEvents(
-          events,
-          MINT_GAME_EVENT_KEY,
-          3
-        );
+        console.log("events", events);
+        const gameId = getNumberValueFromEvents(events, MINT_GAME_EVENT_KEY, 3);
         console.log("Game " + gameId + " minted");
         return gameId;
       } else {
