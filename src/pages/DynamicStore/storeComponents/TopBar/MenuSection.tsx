dc49525cc32efa115f1ddff71fd68c2109281638
brendamareco <brendaamareco@gmail.com>
2025-04-29 23:00:35 -0300

    Adjusted store top bar

--- src/pages/DynamicStore/storeComponents/TopBar/MenuSection.tsx
+++ src/pages/DynamicStore/storeComponents/TopBar/MenuSection.tsx
@@ -1,21 +1,12 @@
 import { Flex, Heading } from "@chakra-ui/react";
 import { MenuBtn } from "../../../../components/Menu/Buttons/MenuBtn";
 import { Icons } from "../../../../constants/icons";
-import { ControllerIcon } from "../../../../icons/ControllerIcon";
 import { useNavigate } from "react-router-dom";
 import { useCurrentPageName } from "../../../../hooks/useCurrentPageName";
-import { useResponsiveValues } from "../../../../theme/responsiveSettings";
-import { SettingsMenuBtn } from "../../../../components/Menu/Buttons/SettingsMenuBtn";
-import { DocsMenuBtn } from "../../../../components/Menu/Buttons/DocsMenuBtn";
-import { LeaderboardMenuBtn } from "../../../../components/Menu/Buttons/LeaderboardMenuBtn";
-import { MapMenuBtn } from "../../../../components/Menu/Buttons/MapMenuBtn";
 
 export const MenuSection = () => {
-  const { isSmallScreen } = useResponsiveValues();
   const page = useCurrentPageName();
   const navigate = useNavigate();
-
-  const iconWidth = "14px";
 
   return (
     <>
@@ -43,17 +34,6 @@ export const MenuSection = () => {
           >
             {page?.name ?? ""}
           </Heading>
-        </Flex>
-        <Flex gap={2} justifyContent={"center"} alignItems={"center"}>
-          <SettingsMenuBtn width={iconWidth} />
-          <DocsMenuBtn width={iconWidth} />
-          <LeaderboardMenuBtn width={iconWidth} />
-          <MapMenuBtn
-            width={iconWidth}
-            mtText={isSmallScreen ? "8px" : undefined}
-            fontSizeText={isSmallScreen ? "6px" : undefined}
-          />
-          <ControllerIcon width={iconWidth} />
         </Flex>
       </Flex>
     </>
