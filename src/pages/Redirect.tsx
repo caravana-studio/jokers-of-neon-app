3ae14acc72910a68cd60169099d7c0b9248cd7b3
brendamareco <brendaamareco@gmail.com>
2025-05-02 16:42:36 -0300

    deleted comments

--- src/pages/Redirect.tsx
+++ src/pages/Redirect.tsx
@@ -1,17 +1,11 @@
-import { useEffect } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { PositionedDiscordLink } from "../components/DiscordLink";
 import { Loading } from "../components/Loading";
 import { useGame } from "../dojo/queries/useGame";
-import { GameStateEnum } from "../dojo/typescript/custom";
 import { getLSGameId } from "../dojo/utils/getLSGameId";
 import { useRedirectByGameState } from "../hooks/useRedirectByGameState";
 
 export const Redirect = () => {
-  const game = useGame();
-  const state = game?.state;
-  const navigate = useNavigate();
-  const { page } = useParams();
   const location = useLocation();
   const lastTabIndex = location.state?.lastTabIndex ?? 0;
 
@@ -20,30 +14,6 @@ export const Redirect = () => {
     { gameId: getLSGameId() },
     { state: { lastTabIndex: lastTabIndex } }
   );
-
-  // useEffect(() => {
-  //   if (state === "GameOver") {
-  //     navigate(`/gameover/${getLSGameId()}`);
-  //   } else if (
-  //     (state === GameStateEnum.Round || state === GameStateEnum.Rage) &&
-  //     page === "demo"
-  //   ) {
-  //     navigate("/demo");
-  //   } else if (state === GameStateEnum.Map && page === "map") {
-  //     navigate("/map");
-  //   } else if (state === GameStateEnum.Store && page === "store") {
-  //     navigate("/store", { state: { lastTabIndex: lastTabIndex } });
-  //   } else if (
-  //     state === GameStateEnum.Lootbox &&
-  //     page === "loot-box-cards-selection"
-  //   ) {
-  //     navigate("/loot-box-cards-selection");
-  //   } else if (page === "state" && state) {
-  //     navigate(stateToPageMap[state as keyof typeof stateToPageMap] ?? "/", {
-  //       replace: true,
-  //     });
-  //   }
-  // }, [state, page, navigate]);
 
   return (
     <>
