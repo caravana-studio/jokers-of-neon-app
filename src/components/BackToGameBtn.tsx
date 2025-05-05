438d68d6c97d63ab8b2da3b2e2b84bab09109883
brendamareco <brendaamareco@gmail.com>
2025-05-05 09:49:08 -0300

    Fix back to game navigation

--- src/components/BackToGameBtn.tsx
+++ src/components/BackToGameBtn.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import { useResponsiveValues } from "../theme/responsiveSettings";
 import { useGame } from "../dojo/queries/useGame";
+import { GameStateEnum } from "../dojo/typescript/custom";
 
 export const BackToGameBtn = () => {
   const { t } = useTranslation(["game"]);
@@ -19,7 +20,7 @@ export const BackToGameBtn = () => {
       fontSize={isSmallScreen ? 10 : [10, 10, 10, 14, 14]}
       onClick={() => {
         if (game) {
-          game.state === "AT_SHOP"
+          game.state === GameStateEnum.Store
             ? navigate("/store", { state: { lastTabIndex: 2 } })
             : navigate("/demo", {
                 state: { skipRageAnimation: true },
