5db0d2fc87b84d5a1887ac3cc05b31e872fea7ee
Cristian Fleita <cristianagustinfleita@gmail.com>
2025-05-16 11:13:18 -0300

    chore: retrieve current hand cards after sync entities

--- src/state/useGameState.tsx
+++ src/state/useGameState.tsx
@@ -196,9 +196,9 @@ export const useGameState = () => {
     }
   }, [dojoHand]);
 
-  useEffect(() => {
-    setHand([]);
-  }, [gameId]);
+  // useEffect(() => {
+  //   setHand([]);
+  // }, [gameId]);
 
   useEffect(() => {
     if (dojoPowerUps?.length > 0 && powerUps.length === 0) {
