9be1cd140fbfdda23d9e969637b35817f6a30a50
Cristian Fleita <87950451+cristianFleita@users.noreply.github.com>
2025-06-18 09:55:05 -0300

    chore: update level passed event (#808)

--- src/types/ScoreData.ts
+++ src/types/ScoreData.ts
@@ -31,7 +31,9 @@ export interface CardPlayEventValue {
 
 export interface LevelPassedEvent {
   level: number;
-  score: number;
+  player_score: number;
+  round: number;
+  level_passed: number;
 }
 
 export interface LevelEvent {
