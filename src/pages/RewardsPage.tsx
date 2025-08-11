42a03cb1b358941d108eeab6385559cf1e0156c8
dub_zn <santiagodgalvan@gmail.com>
2025-08-11 17:32:29 -0300

    remove confetti

--- src/pages/RewardsPage.tsx
+++ src/pages/RewardsPage.tsx
@@ -1,18 +1,12 @@
-import { useEffect } from "react";
 import { BackgroundDecoration } from "../components/Background";
 import { RewardsDetail } from "../components/RewardsDetail";
 import { GameStateEnum } from "../dojo/typescript/custom";
 import { useCustomNavigate } from "../hooks/useCustomNavigate";
 import { useGameStore } from "../state/useGameStore";
-import { runConfettiAnimation } from "../utils/runConfettiAnimation";
 
 export const RewardsPage = () => {
   const { roundRewards } = useGameStore();
   const navigate = useCustomNavigate();
-
-  useEffect(() => {
-    runConfettiAnimation();
-  }, []);
 
   if (!roundRewards) {
     navigate(GameStateEnum.Map);
