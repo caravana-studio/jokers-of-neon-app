791e21ea06d8ec325808499192c6c798f99731e6
nicon44 <nicolas@dulio.com.ar>
2025-08-14 16:39:49 +0200

    delayed loading and decoration

--- src/pages/RewardsPage.tsx
+++ src/pages/RewardsPage.tsx
@@ -1,20 +1,27 @@
 import { BackgroundDecoration } from "../components/Background";
+import { DelayedLoading } from "../components/DelayedLoading";
+import { MobileDecoration } from "../components/MobileDecoration";
 import { RewardsDetail } from "../components/RewardsDetail";
 import { GameStateEnum } from "../dojo/typescript/custom";
 import { useCustomNavigate } from "../hooks/useCustomNavigate";
 import { useGameStore } from "../state/useGameStore";
+import { useResponsiveValues } from "../theme/responsiveSettings";
 
 export const RewardsPage = () => {
   const { roundRewards } = useGameStore();
   const navigate = useCustomNavigate();
+  const { isSmallScreen } = useResponsiveValues();
 
   if (!roundRewards) {
     navigate(GameStateEnum.Map);
   }
 
   return (
-    <BackgroundDecoration>
-      <RewardsDetail roundRewards={roundRewards} />
-    </BackgroundDecoration>
+    <DelayedLoading ms={0}>
+      <BackgroundDecoration>
+        {isSmallScreen && <MobileDecoration />}
+        <RewardsDetail roundRewards={roundRewards} />
+      </BackgroundDecoration>
+    </DelayedLoading>
   );
 };
