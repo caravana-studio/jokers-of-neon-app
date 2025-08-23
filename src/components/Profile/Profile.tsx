0739f6270665fb7a4c72b88f97cd924aa82ec47f
brendamareco <brendaamareco@gmail.com>
2025-08-23 14:53:10 -0300

    Extracted profile page and updated desktop layout

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,97 +1,27 @@
-import {
-  Divider,
-  Flex,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalOverlay,
-} from "@chakra-ui/react";
-import { ProfileStats } from "./ProfileStats";
-import { UserBadges } from "./UserBadges";
-import { LogoutMenuBtn } from "../Menu/Buttons/Logout/LogoutMenuBtn";
-import { DeleteAccBtn } from "../Menu/Buttons/DeleteAccBtn";
-import { ControllerIcon } from "../../icons/ControllerIcon";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { useUsername } from "../../dojo/utils/useUsername";
+import { useResponsiveValues } from "../../theme/responsiveSettings";
 import { profileMock } from "../../utils/mocks/profileMocks";
-import { useDojo } from "../../dojo/DojoContext";
-import { ModalProps } from "../../types/Modals";
+import { ProfileDesktop } from "./ProfileDesktop";
+import { ProfileMobile } from "./ProfileMobile";
 
-export const Profile = ({ close }: ModalProps) => {
-  const username = profileMock.username;
-  const level = profileMock.level;
-  const streak = profileMock.streak;
-  const games = profileMock.games;
-  const victories = profileMock.victories;
-  const currentXp = profileMock.currentXp;
-  const levelXp = profileMock.levelXp;
-  const currentBadges = profileMock.currentBadges;
-  const totalBadges = profileMock.totalBadges;
+export const Profile = () => {
+  const { isSmallScreen } = useResponsiveValues();
 
-  const btnWidth = "18px";
-  const { setup } = useDojo();
+  const profileData: ProfileData = {
+    username: useUsername() ?? profileMock.username,
+    level: profileMock.level,
+    streak: profileMock.streak,
+    games: profileMock.games,
+    victories: profileMock.victories,
+    currentXp: profileMock.currentXp,
+    levelXp: profileMock.levelXp,
+    currentBadges: profileMock.currentBadges,
+    totalBadges: profileMock.totalBadges,
+  };
 
-  return (
-    <>
-      <Modal isOpen={true} onClose={() => {}} isCentered>
-        <ModalOverlay
-          bg="blackAlpha.400"
-          backdropFilter="auto"
-          backdropBlur="5px"
-        />
-        <ModalContent width={"70%"}>
-          <ModalCloseButton onClick={close} />
-          <ModalBody>
-            <ProfileStats
-              username={username}
-              level={level}
-              streak={streak}
-              games={games}
-              victories={victories}
-              currentXp={currentXp}
-              levelXp={levelXp}
-            />
-
-            <Divider borderColor="white" borderWidth="1px" my={2} />
-
-            <Flex
-              gap={4}
-              flexDirection={"row"}
-              justifyContent={"space-between"}
-            >
-              <UserBadges
-                currentBadges={currentBadges}
-                totalBadges={totalBadges}
-              />
-
-              <Flex flexDirection={"column"} gap={2} w={"100%"} color={"white"}>
-                {!setup.useBurnerAcc && (
-                  <>
-                    <Flex justifyContent={"space-between"}>
-                      <LogoutMenuBtn width={btnWidth} label={true} />
-                      <FontAwesomeIcon icon={faArrowRight} />
-                    </Flex>
-                  </>
-                )}
-
-                <Flex justifyContent={"space-between"}>
-                  <DeleteAccBtn width={btnWidth} label={true} />
-                  <FontAwesomeIcon icon={faArrowRight} />
-                </Flex>
-                {!setup.useBurnerAcc && (
-                  <>
-                    <Flex justifyContent={"space-between"}>
-                      <ControllerIcon width={btnWidth} label={true} />
-                      <FontAwesomeIcon icon={faArrowRight} />
-                    </Flex>
-                  </>
-                )}
-              </Flex>
-            </Flex>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-    </>
+  return isSmallScreen ? (
+    <ProfileMobile data={profileData} />
+  ) : (
+    <ProfileDesktop data={profileData} />
   );
 };
