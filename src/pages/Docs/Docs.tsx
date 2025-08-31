891eabc9c7d0e067c6a9918c955abd46232c0344
brendamareco <brendaamareco@gmail.com>
2025-08-31 09:50:57 -0300

    Wait until translation is ready

--- src/pages/Docs/Docs.tsx
+++ src/pages/Docs/Docs.tsx
@@ -5,32 +5,36 @@ import { Tab, TabPattern } from "../../patterns/tabs/TabPattern";
 import { useGameStore } from "../../state/useGameStore";
 import { DocsBoxesRow } from "./DocsBoxesRow";
 import { DocsCardsRow } from "./DocsCardsRow";
+import { Loading } from "../../components/Loading";
 
 interface DocsProps {
   lastIndexTab: number;
 }
 
 export const DocsPage: React.FC<DocsProps> = ({ lastIndexTab = 0 }) => {
-  const { t } = useTranslation(["docs"]);
+  const { t, ready } = useTranslation(["docs"]);
 
   const { modCardsConfig } = useGameStore();
 
-  return (
-    <DelayedLoading>
-      <TabPattern lastIndexTab={lastIndexTab}>
-        <Tab title={t("labels.special-cards")}>
-          <DocsCardsRow cardIds={modCardsConfig?.specialCardsIds ?? []} />
-        </Tab>
-        <Tab title={t("labels.modifier-cards")}>
-          <DocsCardsRow cardIds={Object.keys(MODIFIERS_RARITY).map(Number)} />
-        </Tab>
-        <Tab title={t("labels.rage-cards")}>
-          <DocsCardsRow cardIds={modCardsConfig?.rageCardsIds ?? []} />
-        </Tab>
-        <Tab title={t("labels.loot-boxes")}>
-          <DocsBoxesRow />
-        </Tab>
-      </TabPattern>
-    </DelayedLoading>
-  );
+  if (!ready) {
+    return <Loading />;
+  } else
+    return (
+      <DelayedLoading>
+        <TabPattern lastIndexTab={lastIndexTab}>
+          <Tab title={t("labels.special-cards")}>
+            <DocsCardsRow cardIds={modCardsConfig?.specialCardsIds ?? []} />
+          </Tab>
+          <Tab title={t("labels.modifier-cards")}>
+            <DocsCardsRow cardIds={Object.keys(MODIFIERS_RARITY).map(Number)} />
+          </Tab>
+          <Tab title={t("labels.rage-cards")}>
+            <DocsCardsRow cardIds={modCardsConfig?.rageCardsIds ?? []} />
+          </Tab>
+          <Tab title={t("labels.loot-boxes")}>
+            <DocsBoxesRow />
+          </Tab>
+        </TabPattern>
+      </DelayedLoading>
+    );
 };
