55105744cdc0a556619c40bdb551ca146ebf8643
nicon44 <nicolas@dulio.com.ar>
2025-09-19 15:21:44 +0200

    works with PC

--- src/dojo/controller/controller.ts
+++ src/dojo/controller/controller.ts
@@ -1,11 +1,16 @@
+import ControllerConnector from "@cartridge/connector/controller";
 import SessionConnector from "@cartridge/connector/session";
 import { constants, shortString } from "starknet";
 import { policies } from "./policies";
+import { isNative } from "../../utils/capacitorUtils";
 
 const CHAIN =
   import.meta.env.VITE_SLOT_INSTANCE ||
   import.meta.env.VITE_CHAIN ||
   "jokers-of-neon";
+
+const DOJO_NAMESPACE =
+  import.meta.env.VITE_DOJO_NAMESPACE || "jokers_of_neon_core";
 
 const getChainId = (chain: string) => {
   if (chain === "mainnet") {
@@ -32,11 +37,27 @@ const isDev = import.meta.env.VITE_DEV === "true";
 
 const RPC_URL = import.meta.env.VITE_RPC_URL || "http://localhost:5050";
 
+const controllerOptions = {
+  chains: [{ rpcUrl: RPC_URL }],
+  defaultChainId,
+  preset: "jokers-of-neon",
+  namespace: DOJO_NAMESPACE,
+  policies,
+  slot: undefined,
+};
+
+if (CHAIN !== "mainnet" && CHAIN !== "sepolia") {
+  controllerOptions.slot = CHAIN;
+}
+
 export const controller =
   !isDev &&
-  new SessionConnector({
-    policies,
-    rpc: RPC_URL,
-    chainId: defaultChainId,
-    redirectUrl: typeof window !== "undefined" ? window.location.origin : "",
-  });
+  (!isNative
+    ? new ControllerConnector(controllerOptions)
+    : new SessionConnector({
+        policies,
+        rpc: RPC_URL,
+        chainId: defaultChainId,
+        redirectUrl:
+          typeof window !== "undefined" ? window.location.origin : "",
+      }));
