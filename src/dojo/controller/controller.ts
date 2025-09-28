c284899d541815737d03dd011d2944c0f504c21a
nicon44 <nicolas@dulio.com.ar>
2025-09-28 21:03:10 +0200

    redirect to app

--- src/dojo/controller/controller.ts
+++ src/dojo/controller/controller.ts
@@ -58,5 +58,5 @@ export const controller =
         policies,
         rpc: RPC_URL,
         chainId: defaultChainId,
-        redirectUrl: "https://www.wikipedia.org",
+        redirectUrl: "https://www.jokersofneon.com/open",
       }));
