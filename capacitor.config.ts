9b9fe82533ece6e1026e951bca1504390835c23b
Nicolas Dulio <nicolas@dulio.com.ar>
2025-09-30 18:10:43 +0200

    update title and add pre-launch to icon (#888)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "com.jokersofneon.play",
-  appName: "jokers-of-neon",
+  appName: "Jokers of Neon - Pre Launch",
   webDir: "dist",
 /*   server: {
     url: "http://localhost:5173",
