da642c39e56c6c42b47c2b7d4ba7c60b8b7b845d
nicon44 <nicolas@dulio.com.ar>
2025-10-10 12:01:11 +0200

    add meta sdk

--- android/app/src/main/java/com/jokersofneon/play/MainActivity.java
+++ android/app/src/main/java/com/jokersofneon/play/MainActivity.java
@@ -1,5 +1,16 @@
 package com.jokersofneon.play;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(getApplication());
+    }
+}
