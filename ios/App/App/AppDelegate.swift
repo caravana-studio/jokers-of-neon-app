ebdf37a5407a91cb0849588aa61feb8a3e4ea281
nicon44 <nicolas@dulio.com.ar>
2025-10-13 12:23:35 +0200

    fix meta sdk ios

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -39,12 +39,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) { }
 
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        // Called when the app was launched with a url. Feel free to add additional processing here,
-        // but if you want the App API to support tracking app url opens, make sure to keep this call
-        let handledByCapacitor = ApplicationDelegateProxy.shared.application(app, open: url, options: options)
+    func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
         let handledByFB = ApplicationDelegate.shared.application(app, open: url, options: options)
-        return handledByCapacitor || handledByFB    
+        let handledByCap = ApplicationDelegateProxy.shared.application(app, open: url, options: options)
+        return handledByFB || handledByCap
     }
 
     func application(
