2912e6c4b4be69125a5943d63d9a104cd593baa9
Nicolas Dulio <nicolas@dulio.com.ar>
2025-10-28 14:51:31 +0100

    Jon 1892 api external packs (#939)
    * mintPack
    
    * get cards from pack api
    
    * sort
    
    * packId

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -220,7 +220,7 @@ export const AppRoutes = () => {
         }
       />
       <Route
-        path="/external-pack"
+        path="/external-pack/:packId?"
         element={
           <AnimatedPage>
             <ExternalPack />
