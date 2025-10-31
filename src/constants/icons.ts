e7819f1a476730ff0533641184eff6195c4cbc0f
nicon44 <nicolas@dulio.com.ar>
2025-10-31 22:21:30 +0100

    update icons

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -24,6 +24,8 @@ import TutorialIcon from "../assets/MenuIcons/tutorial.png";
 import GameoverIcon from "../assets/MenuIcons/gameover.png";
 import MapSimpleIcon from "../assets/MenuIcons/map-simple.png";
 import StoreIcon from "../assets/MenuIcons/shop.png";
+import ShopIcon from "../assets/MenuIcons/shop2.png";
+import SeasonIcon from "../assets/MenuIcons/season.png";
 import RoundIcon from "../assets/MenuIcons/round.png";
 import RageIcon from "../assets/MenuIcons/rage.png";
 import HomeIcon from "../assets/MenuIcons/home.png";
@@ -70,6 +72,8 @@ export const Icons = {
   CLOCK: ClockIcon,
   LEADERBOARD: LeaderboardIcon,
   FREEPACK: FreepackIcon,
+  SHOP: ShopIcon,
+  SEASON: SeasonIcon,
 } as const;
 
 export type IconType = keyof typeof Icons;
