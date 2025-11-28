b35e799448b480e93429af5cd77fefb974c71767
nicon44 <nicolas@dulio.com.ar>
2025-11-28 18:03:37 -0300

    fix payments android

--- android/app/src/main/java/com/jokersofneon/play/MainActivity.java
+++ android/app/src/main/java/com/jokersofneon/play/MainActivity.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.revenuecat.purchases.capacitor.PurchasesPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(getApplication());
+        registerPlugin(PurchasesPlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
