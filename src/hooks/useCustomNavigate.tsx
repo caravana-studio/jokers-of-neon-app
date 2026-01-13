314f6377c6e0d33052da51023869447370e8f18a
nicon44 <nicolas@dulio.com.ar>
2026-01-13 17:26:21 -0300

    fix temporal cards

--- src/hooks/useCustomNavigate.tsx
+++ src/hooks/useCustomNavigate.tsx
@@ -1,18 +1,34 @@
 import { useNavigate as useRouterNavigate } from "react-router-dom";
 import { stateToPageMap } from "../constants/redirectConfig";
 import { GameStateEnum } from "../dojo/typescript/custom";
+import { useDojo } from "../dojo/useDojo";
+import { useCardData } from "../providers/CardDataProvider";
 import { useGameStore } from "../state/useGameStore";
+import { useShopStore } from "../state/useShopStore";
 
 export const useCustomNavigate = () => {
   const navigate = useRouterNavigate();
-  const { setState, resetRage } = useGameStore();
+  const {
+    setState,
+    resetRage,
+    refetchSpecialCards,
+    id: gameId,
+    modId,
+  } = useGameStore();
+  const {
+    setup: { client },
+  } = useDojo();
 
   const customNavigate = (state: GameStateEnum) => {
     setState(state);
     if (state !== GameStateEnum.Rage) {
       resetRage();
     }
-    navigate(stateToPageMap[state]);
+    const targetPath = stateToPageMap[state];
+    if (targetPath === "/store") {
+      refetchSpecialCards(client, gameId);
+    }
+    navigate(targetPath);
   };
 
   return customNavigate;
