2885f9268ea2d3ebd552da42282ce21ca35336b3
nicon44 <nicolas@dulio.com.ar>
2026-04-09 10:21:21 +0200

    fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,5 @@
 import { Flex } from "@chakra-ui/react";
 import { ReactNode } from "react";
-import { matchPath, useLocation } from "react-router-dom";
 import { ReactFlowProvider } from "reactflow";
 import { MapProvider } from "../providers/MapProvider";
 import { StoreProvider } from "../providers/StoreProvider";
@@ -15,13 +14,6 @@ import { BottomMenu } from "./Menu/BottomMenu";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
   const { isSmallScreen } = useResponsiveValues();
-  const location = useLocation();
-  const hideBottomSpacing = Boolean(
-    matchPath(
-      { path: "/shop-tier-unlocked/:gameId", end: true },
-      location.pathname
-    )
-  );
 
   return (
     <ReactFlowProvider>
@@ -33,13 +25,11 @@ export const Layout = ({ children }: { children: ReactNode }) => {
             position="relative"
             pt={nativePaddingTop}
             pb={
-              hideBottomSpacing
-                ? "0px"
-                : isNative
-                  ? "80px"
-                  : isSmallScreen
-                    ? "50px"
-                    : "0px"
+              isNative
+                ? "80px"
+                : isSmallScreen
+                  ? "50px"
+                  : "0px"
             }
             flexDirection={isSmallScreen ? "column" : "row"}
             flexGrow={1}
