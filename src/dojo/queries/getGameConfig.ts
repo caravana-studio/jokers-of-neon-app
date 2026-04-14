53e3be53bfc1341593fb26ce52a0eda61ca654f8
nicon44 <nicolas@dulio.com.ar>
2026-04-14 20:18:50 +0200

    Bump app version to 5.0.5 and default game config on errors

--- src/dojo/queries/getGameConfig.ts
+++ src/dojo/queries/getGameConfig.ts
@@ -4,17 +4,22 @@ const toPositiveInt = (value: unknown): number | undefined => {
   return parsed;
 };
 
+const DEFAULT_CONFIG = {
+  maxPowerUpSlots: 4,
+  maxSpecialCards: 7,
+};
+
 export const getGameConfig = async (client: any, playerAddress: string) => {
   try {
-    const tx_result = await client.mods_info_system.getGameConfigForPlayer(
-      playerAddress
-    );
+    const tx_result =
+      await client.mods_info_system.getGameConfigForPlayer(playerAddress);
     return {
       maxPowerUpSlots: toPositiveInt(tx_result.max_power_up_slots),
       maxSpecialCards: toPositiveInt(tx_result.max_special_slots),
     };
   } catch (e) {
-    console.log(e);
+    console.log(e, "returning default config", DEFAULT_CONFIG);
+    return DEFAULT_CONFIG;
   }
   return {};
 };
