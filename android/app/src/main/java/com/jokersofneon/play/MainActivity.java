d0dd922d42293b27092dcf42beb43c286e574a31
Damian Piñones <pinonesdamian@gmail.com>
2026-04-29 09:39:02 -0300

    feat: enable Firebase Analytics and log app instance ID in MainActivity

--- android/app/src/main/java/com/jokersofneon/play/MainActivity.java
+++ android/app/src/main/java/com/jokersofneon/play/MainActivity.java
@@ -1,10 +1,12 @@
 package com.jokersofneon.play;
 
 import android.os.Bundle;
+import android.util.Log;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.community.audio.NativeAudio;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.revenuecat.purchases.capacitor.PurchasesPlugin;
 import com.jokersofneon.play.AppsFlyerBridgePlugin;
 import com.jokersofneon.play.AgeSignalsBridgePlugin;
@@ -22,5 +24,13 @@ public class MainActivity extends BridgeActivity {
         // Play Age Signals for age range compliance in supported regions
         registerPlugin(AgeSignalsBridgePlugin.class);
         super.onCreate(savedInstanceState);
+
+        // Firebase Analytics — explicit enable (mirrors iOS AppDelegate)
+        FirebaseAnalytics fa = FirebaseAnalytics.getInstance(this);
+        fa.setAnalyticsCollectionEnabled(true);
+        Log.i("Firebase", "Analytics enabled. App instance ID requested.");
+        fa.getAppInstanceId().addOnSuccessListener(id ->
+            Log.i("Firebase", "App instance ID: " + id)
+        );
     }
 }
